a18d6beaadcfd8680eb325e6b3dea1c0bb891369
minklim <minklim47@gmail.com>
2023-10-07 18:41:01 +0700

    [EDIT] update color theme

--- src/theme/foundations/colors.ts
+++ src/theme/foundations/colors.ts
@@ -1,7 +1,24 @@
+// Plese refer the color according to the figma design, the comment of each color will match with the color style used in figma. 
+// If you found any color in figma you need but doesn't exist here, please contact Mink
+
 const colors = {
-  mink: {
-    300: "#FF6800",
+  white: "#F6F6F6", // white or nearly white -> ex. color={"white"}
+  brand: {
+    100: "#DEBEF6", // dark accent -> ex. color={"brand.100"}
+    200: "#A533C8", // dark second -> ex. color={"brand.200"}
+    300: "#5F0DBB", // dark primary
+    400: "#200944", // dark background
   },
+  black:
+    "#191919", // black -> ex. color={"black"}
+  grey: {
+    100: "#D9D9D9", // white grey -> ex. color={"grey.100"}
+    200: "#C5C4C7", // grey
+    300: "#A0AEC0", // grey input
+    400: "#242325", // dark grey
+  },
+  red: "#C83333" // red -> ex. color={"red"}
+
 
 } as const;
 
