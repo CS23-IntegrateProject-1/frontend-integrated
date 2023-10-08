cd2033750f34742ae354aaf6d9403a838790cb8c
Chanakarn Limprasertsiri <minklim47@gmail.com>
2023-10-08 16:48:30 +0700

    [ADD] update text style

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,10 +1,18 @@
-import { Box, Flex, Heading, useDisclosure } from "@chakra-ui/react";
-import { HamburgerIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Flex,
+  // Heading,
+  IconButton,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { HamburgerIcon,ArrowBackIcon } from "@chakra-ui/icons";
 import { NavbarContent } from "./NavbarContent";
-import { useState } from "react";
+import { useLocation } from "react-router-dom";
 
 export const Navbar = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const location = useLocation();
+  console.log();
 
   const handleOpenNav = () => {
     onOpen();
@@ -13,18 +21,41 @@ export const Navbar = () => {
     <Flex
       justifyContent={"space-between"}
       alignItems={"center"}
-      height="124px"
+      height="70px"
       padding={"1em"}
+      bg={"brand.400"}
+      shadow={"md"}
     >
-      <Box>
-        {" "}
-        <HamburgerIcon />
-      </Box>
-      <Box>Harmoni</Box>
-      <HamburgerIcon width={"24px"} height={"24px"} onClick={handleOpenNav} />
+      {location.pathname === "/" ? (
+        <Box width={"40px"} height={"40px"} visibility={"hidden"}></Box>
+      ) : (
+        <IconButton
+          aria-label="Notification Page"
+          variant={"unstyled"}
+          icon={
+            <ArrowBackIcon
+              width={"24px"}
+              height={"24px"}
+              // onClick={handleOpenNav}
+            />
+          }
+        />
+      )}
 
-      <Box position={"absolute"} top={"0"} left={"0"}
-      >
+      <Box color="white">Harmoni</Box>
+      <IconButton
+        aria-label="Notification Page"
+        variant={"unstyled"}
+        icon={
+          <HamburgerIcon
+            width={"24px"}
+            height={"24px"}
+            onClick={handleOpenNav}
+          />
+        }
+      />
+
+      <Box position={"absolute"} top={"0"} left={"0"}>
         <NavbarContent isOpen={isOpen} onOpen={onOpen} onClose={onClose} />
       </Box>
     </Flex>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,8 @@ import { Box, Heading } from "@chakra-ui/react";
 
 export const Home = () => {
   return (
-    <Box textStyle={"mink"}>
-      <Heading color={"grey.400"}>Home Page</Heading>
+    <Box textStyle={"h1"}>
+      <Heading color={"white"}>Home Page</Heading>
       {/* edit anything here jaaaaaaaa */}
     </Box>
   );
