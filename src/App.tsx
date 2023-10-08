10dbe8326b47d012931a797b9db82ced883e4a56
minklim <minklim47@gmail.com>
2023-10-09 01:11:32 +0700

    [ADD] add some routes for feature3

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,32 @@
 import {
-  // Route,
+  Route,
   RouterProvider,
-  createBrowserRouter
+  createBrowserRouter,
+  createRoutesFromElements,
 } from "react-router-dom";
 import { PrivateRoutes } from "./routes/PrivateRoutes";
 import { PublicRoutes } from "./routes/PublicRoutes";
 import { checkAuth } from "./functions/checkAuth";
+import { RootLayout } from "./layouts/RootLayout";
+import { Feature3Routes } from "./features/feature03-overview/Feature3Routes";
+import { Test } from "./pages/Test";
 
 const router = createBrowserRouter([
-  checkAuth() ? PrivateRoutes() : {},
-  ...PublicRoutes(),
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      
+      ...Feature3Routes(),
+      
+    ],
+  },
 ]);
 
 function App() {
+  const userType = "customer";
 
   return <RouterProvider router={router} />;
-
 }
 
-export default App
+export default App;
---
+++ src/features/feature03-overview/pages/BarPage.tsx
@@ -0,0 +1,8 @@
+import { Box } from "@chakra-ui/react"
+
+export const BarPage = () => {
+    return (
+    <Box color={"white"}>
+        This is Bar Page
+    </Box>)
+}
