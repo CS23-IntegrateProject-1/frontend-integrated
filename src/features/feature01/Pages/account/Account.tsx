8252a22433642a47495031d62a53b28edac176d6
Kaung101 <khin.tun1@mail.kmutt.ac.th>
2023-10-14 23:06:55 +0700

    [INPROGRESS]account/security
    Still need to fix Chakra default generating padding

--- src/features/feature01/Pages/account/Account.tsx
+++ src/features/feature01/Pages/account/Account.tsx
@@ -1,13 +1,35 @@
-import { Box, Heading, Text } from "@chakra-ui/react";
+import { Box, VStack, StackDivider} from "@chakra-ui/react";
 import { TextStyle } from "../../../../theme/TextStyle";
-import { ButtonComponent } from "../../../../components/buttons/ButtonComponent";
-
-
+import { NavLink } from "react-router-dom";
 export const Account = () => {
+  
+  const links = [
+    {title : "Profile", to : "/setting"},
+    {title : "Update Payment Method", to : "/setting"},
+    {title : "Social Media Accounts", to : "/setting"},
+  ];
+
   return(
-    <Heading style={TextStyle.h1} color={"white"}>
-        {" "}
-        Hello, This is Account Page
-      </Heading>
+    <VStack 
+      textStyle={TextStyle.h1.fontSize} 
+      fontWeight={TextStyle.h1.fontWeight}
+      divider={<StackDivider/>}
+      spacing={4}
+      align={"stretch"}
+    > 
+    {/* Generating link for each item in the array */}
+      {links.map((link) => (
+        <Box>
+            <Box  key={link.title} as={NavLink} to={link.to} pl={6}>
+            {link.title}
+            </Box>
+        </Box>
+        
+      ))}
+
+      {/* Adding blank Box to make the last item at the bottom */}
+      <Box></Box>
+    </VStack>
   );
+    
 };
