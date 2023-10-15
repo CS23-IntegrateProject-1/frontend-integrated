36377b66aa83d58e2e6ad0ea9e5ef86da1dcbc10
IdSausage <kimmv2ddo@gmail.com>
2023-10-15 23:34:42 +0700

    Revert "Revert "[ADD] RedeemCard.tsx""
    This reverts commit b7e4d859ee9d2dbb85fc8e83ffd0fabe827b850c.

---
+++ src/features/feature09/pages/Redeempage/RedeemCard.tsx
@@ -0,0 +1,46 @@
+import { EmailIcon } from '@chakra-ui/icons'
+import { Box, Button, Heading, Stack, Text } from '@chakra-ui/react'
+import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
+import React from 'react'
+
+
+const RedeemCard = () => {
+  return (
+      <Box>
+          <Card
+              direction={{ base: 'column', sm: 'row' }}
+              variant='filled'
+              h="216px"
+              borderRadius="0px"
+              rowGap="0px"
+              display="flex"
+              flexDirection="column"
+              boxShadow="-30px 50px 100px 20px rgba(101, 23, 188, 0.7) inset , 100px -60px 120px 20px rgba(222, 190, 246, 0.2) inset"          
+          >
+
+              <Box display="flex" flexDirection="row" alignItems="flex-start" padding="55px 0px 0px 15px">
+                  <CardBody>
+                      <Heading size='3xl' textShadow="-2px 0px 3px grey">Regular</Heading>
+
+                      <Text py='2' fontWeight="bold">
+                          500/1000 points
+                      </Text>
+                  </CardBody>
+
+                  <Box marginTop="8px" backgroundColor="brand.400" padding="36px 19px" borderTopLeftRadius="125px" borderBottomLeftRadius="125px" borderTopRightRadius="10px" borderBottom="0px">
+                  </Box>
+              </Box>
+
+            <Box display="flex" flexDirection="row" justifyContent="end" alignItems="flex-start" padding="5px 20px">
+                  <Button variant='solid' padding="27px 20px" backgroundColor='brand.300'  color="white"  borderRadius="30px" columnGap="7px">
+                      <EmailIcon />
+                      My rewards
+                  </Button>
+            </Box>
+              
+          </Card>
+      </Box>
+  )
+}
+
+export default RedeemCard
