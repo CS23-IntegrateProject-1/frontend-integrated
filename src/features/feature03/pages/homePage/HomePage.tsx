ae47f106752f0f56f28ad722a3a1318a3787ebb7
Nithit Lertcharoensombat <xXNeonKitsuneXx@gmail.com>
2023-10-18 01:11:06 +0700

    [ADD] Test push "F3 T11"

--- src/features/feature03/pages/homePage/HomePage.tsx
+++ src/features/feature03/pages/homePage/HomePage.tsx
@@ -2,5 +2,5 @@ import { Box } from "@chakra-ui/react";
 import { ButtonComponent } from "../../../../components/buttons/ButtonComponent";
 
 export const HomePage = () => {
-  return <Box>This is Home Page</Box>;
+  return <Box>This is Home Page F3_T11</Box>;
 };
