b20414fe1c1adfc5425c3f0919e1f4c1785c52d8
Nithit Lertcharoensombat <xXNeonKitsuneXx@gmail.com>
2023-10-26 18:11:50 +0700

    [EDIT] Start mock_Integrate
    - Create Integrate for card
    - Add Mock data

--- src/features/feature03/pages/homePage/Footer_HomePage.tsx
+++ src/features/feature03/pages/homePage/Footer_HomePage.tsx
@@ -1,9 +1,5 @@
 import React from "react";
-import {
-  FaFacebookSquare,
-  FaInstagram,
-  FaLine,
-} from "react-icons/fa";
+import { FaFacebookSquare, FaInstagram, FaLine } from "react-icons/fa";
 
 import { Link } from "react-router-dom";
 import { Box, Flex, Text, Grid, Icon } from "@chakra-ui/react";
@@ -11,49 +7,45 @@ import { NavLink } from "react-router-dom";
 
 export const Footer_HomePage = () => {
   return (
-    <Box
-      textAlign="center"
-      justifyContent="center"
-      mt={8}
-    >
-        <Flex textAlign="center" justifyContent="center"  pt={5}>
-            <NavLink to="/IDK_Path">
-                <Box px={10}>
-                    <FaFacebookSquare color="black" fontSize="40px" _hover={{color:"tomato"}}/>
-                </Box>
-            </NavLink>
-            <NavLink to="/IDK_Path">
-                <Box px={10}>
-                    <FaInstagram color='tomato' fontSize="40px"/>
-                </Box>
-            </NavLink>
-            <NavLink to="/IDK_Path">
-                <Box px={10} _hover={{ textDecoration: "underline"}}>
-                    <FaLine color="brand.200" fontSize="40px"/>
-                </Box>
-            </NavLink>
-        </Flex>
+    <Box textAlign="center" justifyContent="center" mt={8}>
+      <Flex textAlign="center" justifyContent="center" pt={5}>
+        <Box px={10}>
+          <a href="https://www.facebook.com/xXNekoLordXx/" target="_blank">
+            <FaFacebookSquare color="white" fontSize="40px" />
+          </a>
+        </Box>
+        <Box px={10}>
+          <a href="https://www.instagram.com/kitsune_ne_cs/" target="_blank">
+            <FaInstagram color="white" fontSize="40px" />
+          </a>
+        </Box>
+        <Box px={10}>
+          <a href="https://line.me/th/" target="_blank">
+            <FaLine color="white" fontSize="40px" />
+          </a>
+        </Box>
+      </Flex>
       <Flex textAlign="center" justifyContent="center">
         <NavLink to="/privacy-policy">
-            <Box _hover={{ textDecoration: "underline"}}>
-                <Text fontSize="sm" fontWeight="normal" pt={5} px={4}>
-                Privacy Policy
-                </Text>
-            </Box>
+          <Box _hover={{ textDecoration: "underline" }}>
+            <Text fontSize="sm" fontWeight="normal" pt={5} px={4}>
+              Privacy Policy
+            </Text>
+          </Box>
         </NavLink>
         <NavLink to="/term-of-service">
-        <Box _hover={{ textDecoration: "underline"}}>
-          <Text fontSize="sm" fontWeight="normal" pt={5} px={4}>
-            Terms of Services
-          </Text>
-        </Box>
+          <Box _hover={{ textDecoration: "underline" }}>
+            <Text fontSize="sm" fontWeight="normal" pt={5} px={4}>
+              Terms of Services
+            </Text>
+          </Box>
         </NavLink>
         <NavLink to="/IDK_Path">
-        <Box _hover={{ textDecoration: "underline"}}>
-          <Text fontSize="sm" fontWeight="normal" pt={5} px={4}>
-            Contact Us
-          </Text>
-        </Box>
+          <Box _hover={{ textDecoration: "underline" }}>
+            <Text fontSize="sm" fontWeight="normal" pt={5} px={4}>
+              Contact Us
+            </Text>
+          </Box>
         </NavLink>
       </Flex>
 
--- src/features/feature03/pages/homePage/HomePage.tsx
+++ src/features/feature03/pages/homePage/HomePage.tsx
@@ -1,11 +1,8 @@
 import { Box, Stack, StackDivider, Text } from "@chakra-ui/react";
 import { ButtonComponent } from "../../../../components/buttons/ButtonComponent";
-import { Center, Square, Circle } from "@chakra-ui/react";
-import { StarIcon } from "@chakra-ui/icons";
 import { NavLink } from "react-router-dom";
 import { SearchBar } from "./F3_Components/SearchBar";
 import { Footer_HomePage } from "./Footer_HomePage";
-
 import {
   Card,
   CardHeader,
@@ -16,12 +13,45 @@ import {
   Button,
   HStack,
   Flex,
+  Center,
+  Square,
+  Circle,
 } from "@chakra-ui/react";
+import { StarIcon } from "@chakra-ui/icons";
+
+import mockR from "../RF3mock.json";
+import mockP from "../PF3mock.json";
+import mockE from "../EF3mock.json";
+
+interface RProps {
+  id: number;
+  name: string;
+  description: string;
+  picR: string;
+}
+
+interface PProps {
+  id: number;
+  name: string;
+  picP: string;
+}
+
+interface EProps {
+  id: number;
+  name: string;
+  date: string;
+  location: string;
+  picE: string;
+}
 
 export const HomePage = () => {
   // const [searchFilter, setSearchFilter] = useState<string>("");
   // const [selectedRestaurant, setSelectedRestaurant] =
   //   useState<RestaurantProps | null>(null);
+
+  const R: RProps[] = mockR;
+  const P: PProps[] = mockP;
+  const E: EProps[] = mockE;
 
   return (
     <Box>
@@ -124,7 +154,7 @@ export const HomePage = () => {
           <Card
             mt={15}
             mx={{ lg: "4", base: "1" }}
-            w={{ lg: "220px", base: "112px" }}
+            w={{ lg: "220px", base: "115px" }}
             h={{ lg: "150px", base: "115px" }}
             _hover={{ bg: "brand.100" }}
           >
@@ -164,17 +194,25 @@ export const HomePage = () => {
                 <StarIcon color="black" />
               </Circle>
             </HStack>
-            <Stack mt={{ lg: "6", base: "3" }} spacing="3" justifyContent={"center"}>
-              <Heading fontSize={{ lg: "2xl", base: "sm" }} textAlign={"center"}>
+            <Stack
+              mt={{ lg: "6", base: "3" }}
+              spacing="3"
+              justifyContent={"center"}
+            >
+              <Heading
+                fontSize={{ lg: "2xl", base: "sm" }}
+                textAlign={"center"}
+              >
                 Chat Bot
               </Heading>
             </Stack>
           </CardBody>
         </Card>
       </Flex>
-
+      {/* ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// */}
       <Text fontSize="50px">Book Seats</Text>
       <SearchBar />
+      {/* ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// */}
       <Text mt={5} fontSize="40px">
         Maps
       </Text>
@@ -185,77 +223,111 @@ export const HomePage = () => {
           borderRadius="lg"
         />
       </NavLink>
+      {/* ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// */}
       <Text mt={5} fontSize="40px">
         Recommended Places
       </Text>
-      
-      <Card maxW="sm" borderRadius='2xl' bg="brand.200">
-        <CardBody>
-          <Image
-            src="https://media.tenor.com/Bv37u6GJKkkAAAAd/bar-feelre.gif"
-            alt="BarButPic not load"
-            borderRadius="lg"
-          />
-          <Stack mt="6" spacing="3">
-            <Heading color="white" size="md">Living room Sofa</Heading>
-            <Text color="grey.200">
-              This sofa is perfect for modern tropical spaces, baroque inspired
-              spaces, earthy toned spaces and for people who love a chic design
-              with a sprinkle of vintage design.
-            </Text>
-          </Stack>
-          <ButtonGroup mt={4} spacing="2">
-            <Button variant="solid" colorScheme="blue">
-              Buy now
-            </Button>
-            <Button variant="ghost" colorScheme="blue">
-              Add to cart
-            </Button>
-          </ButtonGroup>
-        </CardBody>
-      </Card>
+      <Box overflowX="auto">
+        <Box display="flex">
+          {R.filter((R) => R).map((R, index) => (
+            <Card
+              maxW="sm"
+              borderRadius="2xl"
+              bg="brand.200"
+              key={index}
+              marginRight="5"
+            >
+              <CardBody>
+                <Image
+                  src={R.picR}
+                  alt="BarButPic not load"
+                  borderRadius="lg"
+                />
+                <Stack mt="6" spacing="3">
+                  <Heading color="white" size="md">
+                    {R.name}
+                  </Heading>
+                  <Text color="grey.200">{R.description}</Text>
+                </Stack>
+                <ButtonGroup mt={4} spacing="2">
+                  <Button variant="solid" colorScheme="blue">
+                    Buy now
+                  </Button>
+                  <Button variant="ghost" colorScheme="blue">
+                    Add to cart
+                  </Button>
+                </ButtonGroup>
+              </CardBody>
+            </Card>
+          ))}
+        </Box>
+      </Box>
+      {/* ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// */}
       <Text mt={5} fontSize="40px">
         Promotions
       </Text>
-
-      <Card maxW="sm" borderRadius='2xl' bg="brand.200" _hover={{ bg: "brand.300" }}>
-        <CardBody>
-          <Image
-            src="https://i.pinimg.com/originals/b8/78/64/b87864c8958161004082c2ea16aee291.jpg"
-            alt="Promotion_Pic not load"
-            borderRadius="lg"
-          />
-        </CardBody>
-      </Card>
+      <Box overflowX="auto">
+        <Box display="flex">
+          {P.map((P, index) => (
+            <Card
+              key={index}
+              maxW="sm"
+              borderRadius="2xl"
+              bg="brand.200"
+              marginRight="5"
+            >
+              <CardBody>
+                <Image
+                  src={P.picP}
+                  alt="Promotion_Pic not load"
+                  borderRadius="lg"
+                />
+              </CardBody>
+            </Card>
+          ))}
+        </Box>
+      </Box>
+      {/* ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// */}
       <Text mt={5} fontSize="40px">
         Events
       </Text>
-
-      <Card maxW="sm" borderRadius='2xl' bg="brand.200">
-        <CardBody>
-          <Image
-            src="https://phuketeventcompany.b-cdn.net/wp-content/uploads/2021/06/Expert-Event-Management-Your-Trusted-Event-Planner-and-Organizer-in-Bangkok-1.jpg"
-            alt="Event_Pic not load"
-            borderRadius="lg"
-          />
-          <Stack mt="6" spacing="3">
-            <Text color="grey.200">Date that IDK</Text>
-            <Heading color="white" size="md">Living room Sofa</Heading>
-            <Text color="grey.200">@ Where IDK</Text>
-          </Stack>
-          <ButtonGroup mt={4} spacing="2">
-            <Button variant="solid" colorScheme="blue">
-              Buy now
-            </Button>
-            <Button variant="ghost" colorScheme="blue">
-              Add to cart
-            </Button>
-          </ButtonGroup>
-        </CardBody>
-      </Card>
-
+      <Box overflowX="auto">
+        <Box display="flex">
+          {E.map((E, index) => (
+            <Card
+              key={index}
+              maxW="sm"
+              borderRadius="2xl"
+              bg="brand.200"
+              marginRight="5"
+            >
+              <CardBody>
+                <Image
+                  src={E.picE}
+                  alt="Event_Pic not load"
+                  borderRadius="lg"
+                />
+                <Stack mt="6" spacing="3">
+                  <Text color="grey.200">{E.date}</Text>
+                  <Heading color="white" size="md">
+                    {E.name}
+                  </Heading>
+                  <Text color="grey.200">{E.location}</Text>
+                </Stack>
+                <ButtonGroup mt={4} spacing="2">
+                  <Button variant="solid" colorScheme="blue">
+                    Buy now
+                  </Button>
+                  <Button variant="ghost" colorScheme="blue">
+                    Add to cart
+                  </Button>
+                </ButtonGroup>
+              </CardBody>
+            </Card>
+          ))}
+        </Box>
+      </Box>
       <Footer_HomePage />
     </Box>
-    
   );
 };
