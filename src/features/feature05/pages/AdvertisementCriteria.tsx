8a7cdfbec153faf0ead0554ed6c222ced8c897df
realchanatong <88615326+tongcns@users.noreply.github.com>
2023-10-31 21:02:07 +0700

    [ADD] many page again

--- src/features/feature05/pages/AdvertisementCriteria.tsx
+++ src/features/feature05/pages/AdvertisementCriteria.tsx
@@ -1,27 +1,15 @@
 import {
   Box,
   Button,
-  Select,
-  Stack,
   Text,
-  useDisclosure,
 } from "@chakra-ui/react";
 import { TextStyle } from "../../../theme/TextStyle";
-import { Input } from "@chakra-ui/react";
-import { Textarea } from "@chakra-ui/react";
-import { Radio, RadioGroup } from "@chakra-ui/react";
-import {
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
-} from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 
 export const AdvertisementCriteria = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const navigate = useNavigate();
+  const handleClick=()=> { navigate("/advertisement/request");}
+  
   return (
     <Box
       display={"flex"}
@@ -32,18 +20,18 @@ export const AdvertisementCriteria = () => {
     >
       {/* Criteria * */}
       <Box
-        width="50%"
+        width={{base:"90%" ,md:"70%" ,lg:"50%"}}
         minWidth="250px"
-        maxWidth="400px"
+        maxWidth="1000px"
         display="flex"
         flexDirection={"column"}
       >
-          <Text style={TextStyle.h1} color={"white"} paddingBottom={3}>
-            {" "}
-            Advertisement criteria
-          </Text>  
+        <Text style={TextStyle.h1} color={"white"} paddingBottom={3}>
+          {" "}
+          Advertisement criteria
+        </Text>
 
-          {/* 1 */}
+        {/* 1 */}
         <Box paddingBottom={3} display="flex" flexDirection={"column"}>
           <Text style={TextStyle.h2} color={"white"}>
             {" "}
@@ -58,169 +46,150 @@ export const AdvertisementCriteria = () => {
           </Text>
         </Box>
 
-         {/* 2 */}
-         <Box paddingBottom={3} display="flex" flexDirection={"column"}>
+        {/* 2 */}
+        <Box paddingBottom={3} display="flex" flexDirection={"column"}>
           <Text style={TextStyle.h2} color={"white"}>
             {" "}
             Intellectual Property Rights:
           </Text>
           <Text style={TextStyle.h4} color={"white"}>
             {" "}
-            Respect intellectual property rights when creating and using marketing materials. Do not use copyrighted images, logos, or slogans without proper authorization.
+            Respect intellectual property rights when creating and using
+            marketing materials. Do not use copyrighted images, logos, or
+            slogans without proper authorization.
           </Text>
         </Box>
 
-         {/* 3 */}
-         <Box paddingBottom={3} display="flex" flexDirection={"column"}>
+        {/* 3 */}
+        <Box paddingBottom={3} display="flex" flexDirection={"column"}>
           <Text style={TextStyle.h2} color={"white"}>
             {" "}
             Privacy and Data Protection:
           </Text>
           <Text style={TextStyle.h4} color={"white"}>
             {" "}
-            Safeguard customer data and ensure compliance with data protection laws, such as the General Data Protection Regulation (GDPR) in the European Union or equivalent regulations in your region.
+            Safeguard customer data and ensure compliance with data protection
+            laws, such as the General Data Protection Regulation (GDPR) in the
+            European Union or equivalent regulations in your region.
           </Text>
         </Box>
 
-         {/* 4 */}
-         <Box paddingBottom={3} display="flex" flexDirection={"column"}>
+        {/* 4 */}
+        <Box paddingBottom={3} display="flex" flexDirection={"column"}>
           <Text style={TextStyle.h2} color={"white"}>
             {" "}
             Avoid Discriminatory Content:
           </Text>
           <Text style={TextStyle.h4} color={"white"}>
             {" "}
-            Refrain from using discriminatory language or imagery in advertising materials. Promote diversity and inclusivity in your marketing campaigns.
+            Refrain from using discriminatory language or imagery in advertising
+            materials. Promote diversity and inclusivity in your marketing
+            campaigns.
           </Text>
         </Box>
 
-         {/* 5 */}
-         <Box paddingBottom={3} display="flex" flexDirection={"column"}>
+        {/* 5 */}
+        <Box paddingBottom={3} display="flex" flexDirection={"column"}>
           <Text style={TextStyle.h2} color={"white"}>
             {" "}
             Environmental Claims:
           </Text>
           <Text style={TextStyle.h4} color={"white"}>
             {" "}
-            If you make environmental claims (e.g., "eco-friendly," "sustainable"), ensure they are accurate and substantiated. Avoid greenwashing, which involves exaggerating or making false claims about environmental practices.
+            If you make environmental claims (e.g., "eco-friendly,"
+            "sustainable"), ensure they are accurate and substantiated. Avoid
+            greenwashing, which involves exaggerating or making false claims
+            about environmental practices.
           </Text>
         </Box>
 
-         {/* 6 */}
-         <Box paddingBottom={3} display="flex" flexDirection={"column"}>
+        {/* 6 */}
+        <Box paddingBottom={3} display="flex" flexDirection={"column"}>
           <Text style={TextStyle.h2} color={"white"}>
             {" "}
-            Truthful and Transparent Advertising:
+            Pricing Transparency:
           </Text>
           <Text style={TextStyle.h4} color={"white"}>
             {" "}
-            Ensure that all advertising content, whether on your website, social
-            media, flyers, or other platforms, accurately represents your
-            products, services, and promotions. Avoid false or exaggerated
-            claims.
+            Clearly display prices, fees, and any additional charges in your
+            advertising materials. Avoid hidden fees or misleading pricing
+            practices.
           </Text>
         </Box>
 
-         {/* 7 */}
-         <Box paddingBottom={3} display="flex" flexDirection={"column"}>
+        {/* 7 */}
+        <Box paddingBottom={3} display="flex" flexDirection={"column"}>
           <Text style={TextStyle.h2} color={"white"}>
             {" "}
-            Truthful and Transparent Advertising:
+            Social Responsibility:
           </Text>
           <Text style={TextStyle.h4} color={"white"}>
             {" "}
-            Ensure that all advertising content, whether on your website, social
-            media, flyers, or other platforms, accurately represents your
-            products, services, and promotions. Avoid false or exaggerated
-            claims.
+            Use your advertising to promote responsible drinking and encourage
+            designated drivers or alternative transportation options, especially
+            in bars and clubs.
           </Text>
         </Box>
 
-         {/* 8 */}
-         <Box paddingBottom={3} display="flex" flexDirection={"column"}>
+        {/* 8 */}
+        <Box paddingBottom={3} display="flex" flexDirection={"column"}>
           <Text style={TextStyle.h2} color={"white"}>
             {" "}
-            Truthful and Transparent Advertising:
+            Avoid Offensive Content:
           </Text>
           <Text style={TextStyle.h4} color={"white"}>
             {" "}
-            Ensure that all advertising content, whether on your website, social
-            media, flyers, or other platforms, accurately represents your
-            products, services, and promotions. Avoid false or exaggerated
-            claims.
+            Steer clear of offensive or controversial content that could
+            alienate potential customers or harm your brand's reputation.
           </Text>
         </Box>
 
-         {/* 9 */}
-         <Box paddingBottom={3} display="flex" flexDirection={"column"}>
+        {/* 9 */}
+        <Box paddingBottom={3} display="flex" flexDirection={"column"}>
           <Text style={TextStyle.h2} color={"white"}>
             {" "}
-            Truthful and Transparent Advertising:
+            Community Engagement:
           </Text>
           <Text style={TextStyle.h4} color={"white"}>
             {" "}
-            Ensure that all advertising content, whether on your website, social
-            media, flyers, or other platforms, accurately represents your
-            products, services, and promotions. Avoid false or exaggerated
-            claims.
+            Engage with the local community in a positive way through your
+            advertising efforts. Highlight community involvement, sponsorships,
+            and support for local charities or causes.
           </Text>
         </Box>
 
-         {/* 10 */}
-         <Box paddingBottom={3} display="flex" flexDirection={"column"}>
+        {/* 10 */}
+        <Box paddingBottom={3} display="flex" flexDirection={"column"}>
           <Text style={TextStyle.h2} color={"white"}>
             {" "}
-            Truthful and Transparent Advertising:
+            Monitor User-Generated Content:
           </Text>
           <Text style={TextStyle.h4} color={"white"}>
             {" "}
-            Ensure that all advertising content, whether on your website, social
-            media, flyers, or other platforms, accurately represents your
-            products, services, and promotions. Avoid false or exaggerated
-            claims.
+            Keep an eye on user-generated content related to your business on
+            the platform and review sites. Address any inappropriate or
+            misleading content promptly.
           </Text>
         </Box>
 
-      <Box
-        width="50%"
-        minWidth="250px"
-        maxWidth="400px"
-        display="flex"
-        flexDirection={"row"}
-        paddingBottom={3}
-        justifyContent={"space-evenly"}
-      >
-        <Button
-          backgroundColor="#A533C8"
-          variant="solid"
-          width="40%"
-          color="white"
-          onClick={onOpen}
+        <Box
+          display="flex"
+          flexDirection={"row"}
+          paddingBottom={3}
+          justifyContent={"space-evenly"}
         >
-          Create
-        </Button>
-        <Modal isOpen={isOpen} onClose={onClose}>
-          <ModalOverlay />
-          <ModalContent bgColor={"#DEBEF6"} color={"#200944"}>
-            <ModalHeader mt={3}>The request has been approved</ModalHeader>
-            <ModalCloseButton />
-            <ModalFooter>
-              <Button bgColor={"white"} color={"#200944"} mr={5} width="30%">
-                Print
-              </Button>
-              <Button
-                bgColor={"#A533C8"}
-                mr={3}
-                onClick={onClose}
-                color={"white"}
-                width="30%"
-              >
-                Send Mail
-              </Button>
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
-      </Box>
+          <Button 
+            backgroundColor="#A533C8"
+            variant="solid"
+            width="40%"
+            maxWidth={"200px"}
+            color="white"
+            onClick={handleClick}
+          >
+            Create
+          </Button>
+
+        </Box>
       </Box>
     </Box>
   );
--- src/features/feature05/pages/AdvertisementRequest.tsx
+++ src/features/feature05/pages/AdvertisementRequest.tsx
@@ -1,18 +1,20 @@
-import { Box, Button, Input, Text, useDisclosure } from "@chakra-ui/react";
+import { Box, Button, Select, Stack, Text, useDisclosure} from "@chakra-ui/react";
 import { TextStyle } from "../../../theme/TextStyle";
-import { Textarea } from '@chakra-ui/react'
+import { Input } from "@chakra-ui/react";
+import { Textarea } from "@chakra-ui/react";
+import { Radio, RadioGroup } from "@chakra-ui/react";
 import {
-     Modal,
-     ModalOverlay,
-     ModalContent,
-     ModalHeader,
-     ModalFooter,
-     ModalBody,
-     ModalCloseButton,
-   } from '@chakra-ui/react'
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from '@chakra-ui/react'
 
 export const AdvertisementRequest = () => {
-     const { isOpen, onOpen, onClose } = useDisclosure()
+  const { isOpen, onOpen, onClose } = useDisclosure()
   return (
     <Box
       display={"flex"}
@@ -21,6 +23,8 @@ export const AdvertisementRequest = () => {
       alignItems={"center"}
       width={"100%"}
     >
+
+      {/* Name * */}
       <Box
         paddingBottom={3}
         width="50%"
@@ -29,11 +33,137 @@ export const AdvertisementRequest = () => {
         display="flex"
         flexDirection={"column"}
       >
-        <Text style={TextStyle.h2} color={"white"} paddingBottom={2}>
+        <Text style={TextStyle.h2} color={"white"}>
           {" "}
-          Why is the request rejected?
+          Name *
         </Text>
-        <Textarea placeholder='Description' variant='filled' height={"150px"}/>
+        <Input
+          variant="name"
+          placeholder="Filled"
+          style={{ width: "auto" }}
+          color={"black"}
+        />
+      </Box>
+
+      {/* Description * */}
+      <Box
+        paddingBottom={3}
+        width={"50%"}
+        minWidth={"250px"}
+        maxWidth={"400px"}
+        display={"flex"}
+        flexDirection={"column"}
+      >
+        <Text style={TextStyle.h2} color={"white"}>
+          {" "}
+          Description *
+        </Text>
+        <Textarea
+          variant="name"
+          placeholder="Filled"
+          width="auto"
+          color={"black"}
+        />
+      </Box>
+
+      {/* Starting Date * & Ending Date * */}
+      <Box
+        paddingBottom={3}
+        width={"50%"}
+        minWidth={"250px"}
+        maxWidth={"400px"}
+        display={"flex"}
+        flexDirection={"row"}
+        justifyContent={"center"}
+      >
+        <Box mr={"20px"} flex={"1"}>
+          <Text style={TextStyle.h2} color={"white"}>
+            {" "}
+            Starting Date *
+          </Text>
+          <Input size={"xs"} type="date" color="black" bg={"white"}></Input>
+        </Box>
+        <Box flex={"1"}>
+          <Text style={TextStyle.h2} color={"white"}>
+            {" "}
+            Ending Date *
+          </Text>
+          <Input size={"xs"} type="date" color="black" bg={"white"}></Input>
+        </Box>
+      </Box>
+
+      {/* Type */}
+      <Box
+        width="50%"
+        minWidth="250px"
+        maxWidth="400px"
+        display="flex"
+        flexDirection={"column"}
+        paddingBottom={3}
+      >
+        <Text style={TextStyle.h2} color={"white"}>
+          {" "}
+          Type
+        </Text>
+        <RadioGroup defaultValue="2">
+          <Stack spacing={1} direction="column">
+            <Radio value="1">Special offers/ promotions</Radio>
+            <Radio value="2">Special events</Radio>
+          </Stack>
+        </RadioGroup>
+      </Box>
+
+      <Box
+        width="50%"
+        minWidth="250px"
+        maxWidth="400px"
+        display="flex"
+        flexDirection={"column"}
+        paddingBottom={10}
+      >
+        <Text style={TextStyle.h2} color={"white"} paddingBottom={1}>
+          {" "}
+          Images (mobile & desktop view)
+        </Text>
+        <Stack spacing={2} direction="column">
+          <Box width={"auto"} height={"100"} bg={"white"} />
+          <Box width={"200"} height={"100"} bg={"white"} />
+        </Stack>
+      </Box>
+
+      <Box
+        width="50%"
+        minWidth="250px"
+        maxWidth="400px"
+        display="flex"
+        flexDirection={"column"}
+        paddingBottom={5}
+      >
+        <Text style={TextStyle.h2} color={"white"}>
+          {" "}
+          Advertisement plan
+        </Text>
+        <RadioGroup defaultValue="2">
+          <Stack spacing={1} direction="column">
+            <Radio value="1">100 Baht/Week</Radio>
+            <Radio value="2">300 Baht/Month</Radio>
+            <Radio value="3">3,600 Baht/Year</Radio>
+          </Stack>
+        </RadioGroup>
+      </Box>
+
+      <Box
+        width="50%"
+        minWidth="250px"
+        maxWidth="400px"
+        display="flex"
+        flexDirection={"column"}
+        paddingBottom={8}
+      >
+        <Select placeholder="Payment method" style={TextStyle.h1}>
+          <option value="option1">Prompt pay</option>
+          <option value="option2">Cash</option>
+        </Select>
       </Box>
 
       <Box
@@ -43,16 +173,25 @@ export const AdvertisementRequest = () => {
         display="flex"
         flexDirection={"row"}
         paddingBottom={3}
-        justifyContent={"right"}
+        justifyContent={"space-evenly"}
       >
+        <Button 
+        colorScheme="gray" 
+        variant="solid" 
+        width="40%" 
+        color="#A533C8"
+        >
+          Reject
+        </Button>
+
         <Button 
           backgroundColor="#A533C8"
           variant="solid"
-          width="150px"
+          width="40%"
           color="white"
           onClick={onOpen}
         >
-          Send Feedback
+          Accept
         </Button>
         <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
@@ -68,7 +207,6 @@ export const AdvertisementRequest = () => {
         </ModalContent>
       </Modal>
       </Box>
-
     </Box>
   );
 };
