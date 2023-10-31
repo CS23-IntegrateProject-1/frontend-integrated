dda94ba5e0e983e25a05a904841b644da77a4f75
Chayapol Mahatthanachai <best37@hotmail.com>
2023-10-31 09:56:55 +0700

    Merge branch 'Alpha' into feature-05

--- src/features/feature01/Feature01Routes.tsx
+++ src/features/feature01/Feature01Routes.tsx
@@ -1,32 +1,50 @@
+import { SettingHomepage } from './Pages/homepage/SettingHomepage'
+import { Account } from './Pages/account/Account'
+import { Notifications } from './Pages/notifications/Notifications'
+import { PrivacyPolicy } from './Pages/privacyPolicy/PrivacyPolicy'
+import { TermOfService } from './Pages/termOfService/TermOfService'
+import { Help } from './Pages/help/Help'
+import { About } from './Pages/about/About'
+import { Profile } from './Pages/profile/Profile'
+import { PaymentMethodSetting } from './Pages/paymentMethodSetting/PaymentMethodSetting'
 export const Feature01Routes = () => {
   return [
     {
       path: "/setting",
-      element: "ElementPlaceHolder",
+      element: <SettingHomepage/>,
     },
     {
       path: "/setting/account",
-      element: "ElementPlaceHolder",
+      element: <Account/>,
+    },
+    {
+      path: "/setting/account/profile",
+      element: <Profile/>,
+    },
+    
+    {
+      path: "/setting/account/paymentmethodsetting",
+      element: <PaymentMethodSetting/>,
     },
     {
       path: "/setting/notifications",
-      element: "ElementPlaceHolder",
+      element: <Notifications/>,
     },
     {
       path: "/setting/privacy-policy",
-      element: "ElementPlaceHolder",
+      element: <PrivacyPolicy/>,
     },
     {
       path: "/setting/term-of-service",
-      element: "ElementPlaceHolder",
+      element: <TermOfService/>,
     },
     {
       path: "/setting/help",
-      element: "ElementPlaceHolder",
+      element: <Help/>,
     },
     {
       path: "/setting/about",
-      element: "ElementPlaceHolder",
+      element: <About/>,
     },
   ];
 };
