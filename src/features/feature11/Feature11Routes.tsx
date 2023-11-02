4e7d308b42beb968e78293e04914a8be720cbedd
nonsomroop <somroopnatthanon@gmail.com>
2023-11-02 20:01:21 +0700

    [ADD] Create Article page

--- src/features/feature11/Feature11Routes.tsx
+++ src/features/feature11/Feature11Routes.tsx
@@ -1,10 +1,12 @@
 import { CommunityPage } from "./page/CommunityPage";
+import { CreateArticlePage } from "./page/CreateArticlePage";
 
 export const Feature11Routes = () => {
   return [
     { path: "/article", element: "ElementPlaceHolder" },
     { path: "/article/:articleId", element: "ElementPlaceHolder" },
     { path: "/community", element: <CommunityPage /> },
+    { path: "/article/create", element: <CreateArticlePage />}
 
   ];
 };
