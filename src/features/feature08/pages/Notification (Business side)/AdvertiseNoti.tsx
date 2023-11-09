16016fbab26a5332094aeb988e0f1b96d56931c4
3hiroha <97663526+3hiroha@users.noreply.github.com>
2023-11-09 15:20:17 +0700

    [UPDATE] AdvertiseNoti.tsxt
    -Improve the support for phone
    -Centering the box

--- src/features/feature08/pages/Notification (Business side)/AdvertiseNoti.tsx
+++ src/features/feature08/pages/Notification (Business side)/AdvertiseNoti.tsx
@@ -1,69 +1,76 @@
 import {
-    Table,
-    Tbody,
-    Tr,
-    Td,
-    TableContainer,
-    Card,
-    Heading,
-    CardBody,
-    Stack,
-    StackDivider,
-    Box,
-    Text
-  } from "@chakra-ui/react";
+  Table,
+  Tbody,
+  Tr,
+  Td,
+  TableContainer,
+  Card,
+  Heading,
+  CardBody,
+  Stack,
+  StackDivider,
+  Box,
+  Text,
+  Center
+} from "@chakra-ui/react";
 
 import { ButtonComponent } from "../../../../components/buttons/ButtonComponent";
 
 export const AdvertiseNoti = () => {
-    return (
-        <Box
-      display={"flex"}
-      flexDirection={"column"}
-      justifyContent={"center"}
-      alignItems={"center"}
-      margin={5}
-      width={"50%"}
-    >
-      <Card width={"70%"} backgroundColor={"#5F0DBB"} color={"#C5C4C7"}>
-        <CardBody>
-          <Stack divider={<StackDivider />} spacing="4">
-            <Box>
-              <Heading size="md">
-                Title : Advertisement approved
-              </Heading>
-              <Text>
-                This email is to notify you that your 
-                advertisement has been improved and 
-                this is the total amount that you have to pay. 
-              </Text>
-              <Heading size="md">
-                Payment Information
-              </Heading>
-              <TableContainer>
-                <Table variant={'striped'} colorScheme="pink">
+  return (
+    <Center>
+      <Box
+        display={"flex"}
+        flexDirection={"column"}
+        justifyContent={"center"}
+        alignItems={"center"}
+        margin={[3, 5, 7]} // Responsive margin for different screen sizes
+        width={["100%", "80%", "70%"]} // Responsive width for different screen sizes
+      >
+        <Card width={"100%"} backgroundColor={"#5F0DBB"} color={"#C5C4C7"}>
+          <CardBody>
+            <Stack divider={<StackDivider />} spacing={[2, 4]}>
+              <Box>
+                <Heading size="md" textAlign="center">
+                  Title: Advertisement approved
+                </Heading>
+                <Text textAlign="center">
+                  This email is to notify you that your advertisement has been improved,
+                  and this is the total amount that you have to pay.
+                </Text>
+                <Heading size="md" textAlign="center">
+                  Payment Information
+                </Heading>
+                <TableContainer>
+                  <Table
+                    className="striped-table"
+                    variant={'striped'}
+                    colorScheme="pink"
+                    width="100%"
+                  >
                     <Tbody>
-                        <Tr>
-                            <Td>Discription</Td>
-                            <Td>Amount</Td>
-                        </Tr>
-                        <Tr>
-                            <Td>Advertisement</Td>
-                            <Td>1000 baht</Td>
-                        </Tr>
+                      <Tr>
+                        <Td>Discription</Td>
+                        <Td>Amount</Td>
+                      </Tr>
+                      <Tr>
+                        <Td>Advertisement</Td>
+                        <Td>1000 baht</Td>
+                      </Tr>
                     </Tbody>
-                </Table>
-              </TableContainer>
-              <Text align={"right"}>
-                Total cost : 1000 baht
-              </Text>
-              <Text align={"center"}>
-                <ButtonComponent text="Pay now" />
-              </Text>
-            </Box>
-          </Stack>
-        </CardBody>
-      </Card>
-    </Box>
-    )
-}
+                  </Table>
+                </TableContainer>
+                <Text textAlign="right">
+                  Total cost: 1000 baht
+                </Text>
+                <Text textAlign="center">
+                  <ButtonComponent text="Pay now" />
+                </Text>
+              </Box>
+            </Stack>
+          </CardBody>
+        </Card>
+      </Box>
+    </Center>
+  );
+};
