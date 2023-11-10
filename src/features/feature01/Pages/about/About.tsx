78c718167844255baccf9bcea0d84261b35001ba
watharphat <99311004+watharphat@users.noreply.github.com>
2023-11-10 11:51:17 +0700

    [ADD] Help & About pages
    About page done.
    Help page need carousel and some stylings.

--- src/features/feature01/Pages/about/About.tsx
+++ src/features/feature01/Pages/about/About.tsx
@@ -1,12 +1,43 @@
-import { Box, Heading, Text } from "@chakra-ui/react";
+import { Box, Divider, Flex, Grid, GridItem, Heading, Text, VStack, HStack, Container } from "@chakra-ui/react";
 import { TextStyle } from "../../../../theme/TextStyle";
 import { ButtonComponent } from "../../../../components/buttons/ButtonComponent";
+import harmoni_logo from "../../../../assets/logo/harmoni_logo.png";
+import textStyles from "../../../../theme/foundations/textStyles";
+import colors from "../../../../theme/foundations/colors";
 
 
 export const About = () => {
   return (
-      <Box width={"300px"} height={"150px"} bg={"brand.300"} m={"32px"} color={"white"}>
-          This is About page.
-      </Box>
+
+    <Grid>
+      <GridItem width={"25%"} margin={"auto"}>
+           <img src = {harmoni_logo} alt = {"logo"}/>
+      </GridItem>
+      <GridItem style={{textAlign: "center"}}>
+      <Text
+      fontSize={[textStyles.h2.fontSize]}
+      fontWeight={[textStyles.h2.fontWeight]}
+      color={"brand.100"}>
+        {" "}
+        V1.0.0.0
+      </Text>
+      </GridItem>
+      <GridItem>     
+      <HStack>
+        <Heading 
+        fontSize={[textStyles.h1.fontSize]}
+        fontWeight={[textStyles.h1.fontWeight]}>
+        Harmoni
+      </Heading>
+      <Divider border={"2px"} color={"brand.100"} />
+      </HStack> 
+      </GridItem>
+      <GridItem 
+      fontSize={[textStyles.body1.fontSize]}
+      fontWeight={[textStyles.body1.fontWeight]}>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio repudiandae ipsa molestiae neque iure eius ratione illum reprehenderit autem eaque. Atque exercitationem officia, sapiente numquam error libero corrupti aspernatur dolore eligendi maiores maxime necessitatibus laborum nostrum laboriosam consectetur at facere sit unde ullam nemo incidunt veniam temporibus. Deserunt, soluta doloribus.
+      </GridItem>
+    </Grid>
+
   )
 }
--- src/features/feature01/Pages/help/Help.tsx
+++ src/features/feature01/Pages/help/Help.tsx
@@ -1,13 +1,97 @@
-import { Box, Heading, Text } from "@chakra-ui/react";
+import { Box, Grid, GridItem, Heading, Text,HStack, Divider, Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon, } from "@chakra-ui/react";
 import { TextStyle } from "../../../../theme/TextStyle";
 import { ButtonComponent } from "../../../../components/buttons/ButtonComponent";
-
+import textStyles from "../../../../theme/foundations/textStyles";
 
 export const Help = () => {
   return(
-    <Heading style={TextStyle.h1} color={"white"}>
-        {" "}
-        Hello, This is Help Page
+    <Grid>
+      <GridItem>
+        <Box p={"20px"} bgColor={"white"}>
+        <Text color={"black"} textAlign={"center"}>
+          Carousel
+        </Text>
+        </Box>
+      </GridItem>
+
+      <GridItem marginTop={"20px"}>
+      <HStack>
+        <Heading 
+        style={TextStyle.h2}>
+        FAQs
       </Heading>
+      <Divider border={"2px"} color={"brand.100"} />
+      </HStack> 
+      </GridItem>
+{/* Accordion 1 */}
+      <GridItem>
+      <Accordion borderColor={"grey.200"} marginTop={"20px"} marginBottom={"20px"}>
+          <AccordionItem >
+            <h2>
+              <AccordionButton  _expanded={{ bg: 'brand.200', color: 'white' }} borderColor={"grey.200"}>
+                <Box as="span" flex='1' textAlign='left' borderColor={"grey.200"}
+                style={TextStyle.h4}>
+                  {" "}
+                  FAQ #1
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4} bgColor={"brand.200"} opacity={"50%"} style={TextStyle.body2} >
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+              commodo consequat.
+            </AccordionPanel>
+          </AccordionItem>
+
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box as="span" flex='1' textAlign='left'>
+                  {" "}
+                  FAQ #2
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+              commodo consequat.
+            </AccordionPanel>
+          </AccordionItem>
+        </Accordion>
+      </GridItem>
+
+      <GridItem marginTop={"10px"}>
+      <HStack>
+        <Heading 
+        style={TextStyle.h2}>
+        Contact
+      </Heading>
+      <Divider border={"2px"} color={"brand.100"} />
+      </HStack> 
+      </GridItem>
+      <GridItem marginTop={"20px"} >
+        <Box border={"1px"}  borderColor={"brand.200"} bg = 'brand.300' >
+          <Text padding={"10px"} style={TextStyle.h4}>
+            Call Center
+          </Text>
+        </Box>
+      </GridItem>
+      <GridItem>
+        <Box border={"1px"}  borderColor={"brand.200"} bg = 'brand.300' >
+          <Text padding={"10px"} style={TextStyle.h4}>
+            Live message with ChatBox
+          </Text>
+        </Box>
+      </GridItem>
+    </Grid>
   );
 };
