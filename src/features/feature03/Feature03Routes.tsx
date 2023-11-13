fe15686a1be82ff23bf45f07ce6bf1de062ea738
Nithit Lertcharoensombat <xXNeonKitsuneXx@gmail.com>
2023-11-13 21:23:00 +0700

    [ADD] ResturantDetail Temp
    - D

--- src/features/feature03/Feature03Routes.tsx
+++ src/features/feature03/Feature03Routes.tsx
@@ -5,6 +5,7 @@ import { PromotionsPage } from "./pages/homePage/PromotionsPage";
 import { RecommendedPlacesPage } from "./pages/homePage/RecommendedPlacesPage";
 import { RestaurantPage } from "./pages/homePage/RestaurantPage";
 import { ReviewPage } from "./pages/homePage/ReviewPage";
+import { Temp_ResturantDetail } from "./pages/homePage/Temp_ResturantDetail";
 
 export const Feature03Routes = () => {
   return [
@@ -15,6 +16,8 @@ export const Feature03Routes = () => {
     { path: "/Bars", element: <BarsPage /> },
     { path: "/RecommendedPlaces", element: <RecommendedPlacesPage /> },
     { path: "/Promotions", element: <PromotionsPage /> },
+
+    { path: "/Temp_ResturantDetail", element: <Temp_ResturantDetail /> },
 
     { path: "/venue-list", element: "ElementPlaceHolder" },
     { path: "/venue-list/:venueId", element: "ElementPlaceHolder" },
---
+++ src/features/feature03/pages/homePage/Temp_ResturantDetail.tsx
@@ -0,0 +1,22 @@
+import { NavLink } from "react-router-dom";
+import {
+  Box,
+  Stack,
+  Text,
+  Card,
+  CardBody,
+  Heading,
+  Image,
+  Button,
+  Flex,
+} from "@chakra-ui/react";
+import { StarIcon } from "@chakra-ui/icons";
+
+
+export const Temp_ResturantDetail = () => {
+  return (
+    <Box width={"100%"}>
+      Temp_TesturantDetail components
+    </Box>
+  );
+};
