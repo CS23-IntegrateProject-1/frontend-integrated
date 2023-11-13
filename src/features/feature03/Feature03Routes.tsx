81bba2bad5dd50df1a60d4600c74784531db7d15
Snailoba <fameeverywhere@gmail.com>
2023-11-13 22:40:59 +0700

    [ADD] Restaurant detail
    -Restaurant details
    -Design still changing

--- src/features/feature03/Feature03Routes.tsx
+++ src/features/feature03/Feature03Routes.tsx
@@ -17,7 +17,7 @@ export const Feature03Routes = () => {
     { path: "/RecommendedPlaces", element: <RecommendedPlacesPage /> },
     { path: "/Promotions", element: <PromotionsPage /> },
 
-    { path: "/Temp_ResturantDetail", element: <Temp_ResturantDetail /> },
+    { path: "/Temp_RestaurantDetail", element: <Temp_ResturantDetail /> },
 
     { path: "/venue-list", element: "ElementPlaceHolder" },
     { path: "/venue-list/:venueId", element: "ElementPlaceHolder" },
