d1916a28325d7354e5305258f77a75a98b80159e
3hiroha <97663526+3hiroha@users.noreply.github.com>
2023-11-15 14:48:44 +0700

    [Edit] AdvertiseNoti

--- src/features/feature08/pages/Notification (Business side)/AdvertiseNoti.tsx
+++ src/features/feature08/pages/Notification (Business side)/AdvertiseNoti.tsx
@@ -15,8 +15,43 @@ import {
 } from "@chakra-ui/react";
 
 import { ButtonComponent } from "../../../../components/buttons/ButtonComponent";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { AdvertisementID } from "../../../feature05/pages/AdvertisementID";
+
+type AdvertiseNoti = {
+  advertisementId: string;
+  name: string;
+  description: string;
+  image_url: string;
+  start_date: string;
+  end_date: string;
+  cost: string;
+  isApprove: string;
+  customer_type: string;
+  target_group: string;
+  businessId: string;
+}
+
+
 
 export const AdvertiseNoti = () => {
+
+  const [advertiseNotified, setAdvertiseNotified] = useState<AdvertiseNoti[]>([]);
+
+  useEffect(() =>{
+    axios
+        .get(`http://localhost:8080/feature8/advertisements`)
+        .then((res) =>{
+          // console.log(res.data)
+          setAdvertiseNotified(res.data)
+        })
+  })
+  
+
+
+
+
   return (
     <Center>
       <Box
@@ -25,49 +60,69 @@ export const AdvertiseNoti = () => {
         margin={[3, 5, 7]} // Responsive margin for different screen sizes
         width={["100%", "80%", "70%"]} // Responsive width for different screen sizes
       >
-        <Card width={"100%"} backgroundColor={""} color={"white"} border={"1px solid #DEBEF6"}> 
-          <CardBody>
-            <Stack divider={<StackDivider />} spacing={[2, 4]}>
-              <Box>
-                <Heading size="md" marginBottom={3}>
-                  Title: Advertisement approved
-                </Heading>
-                <Text marginBottom={5}>
-                  This email is to notify you that your advertisement has been improved,
-                  and this is the total amount that you have to pay.
-                </Text>
-                <Heading size="md" marginBottom={2}>
-                  Payment Information
-                </Heading>
-                <TableContainer>
-                  <Table
-                    className="striped-table"
-                    variant={'striped'}
-                    colorScheme=""
-                    width="100%"
-                  >
-                    <Tbody>
-                      <Tr backgroundColor={"#A533C8"}>
-                        <Td>Discription</Td>
-                        <Td textAlign={"right"}>Amount</Td>
-                      </Tr>
-                      <Tr>
-                        <Td>Advertisement</Td>
-                        <Td textAlign={"right"}>1000 baht</Td>
-                      </Tr>
-                    </Tbody>
-                  </Table>
-                </TableContainer>
-                <Text marginTop={2} textAlign={"right"}>
-                  Total cost: 1000 baht
-                </Text>
-                <Text marginTop={5} textAlign={"center"}>
-                  <ButtonComponent text="Pay now" />
-                </Text>
-              </Box>
-            </Stack>
-          </CardBody>
-        </Card>
+        {advertiseNotified.map((advertise) => (
+          
+            <Card key={advertise.advertisementId} 
+                  width={"100%"} 
+                  backgroundColor={""} 
+                  color={"white"} 
+                  border={"1px solid #DEBEF6"}
+                  marginBottom={5}> 
+            <CardBody>
+              <Stack divider={<StackDivider />} spacing={[2, 4]}>
+                <Box>
+                  <Heading size="md" marginBottom={3}>
+                    {/* Title: Advertisement approved */}
+                    Title: Advertisement {advertise.isApprove === "Completed" ? "approved" : (advertise.isApprove === "In_progress" ? "in_progress" : (advertise.isApprove === "Rejected" ? "Rejected" : "other status"))}
+
+                  </Heading>
+                  <Text marginBottom={5}>
+                    This email is to notify you that your advertisement has been {advertise.isApprove === "Completed" ? "approved and this is the total amount that you have to pay." 
+                                                                                : (advertise.isApprove === "In_progress" ? "in_progress and this is the estimated total amount that you have to pay." 
+                                                                                : (advertise.isApprove === "Rejected" ? "Rejected." : "other status."))}
+                  </Text>
+                  <Heading size="md" marginBottom={2}>
+                    Payment Information
+                  </Heading>
+                  <TableContainer>
+                    <Table
+                      className="striped-table"
+                      variant={'striped'}
+                      colorScheme=""
+                      width="100%"
+                    >
+                      <Tbody>
+                        <Tr backgroundColor={"#A533C8"}>
+                          <Td>Discription</Td>
+                          <Td textAlign={"right"}>Amount</Td>
+                        </Tr>
+                        <Tr>
+                          <Td>Advertisement</Td>
+                          <Td textAlign={"right"}>
+                            {/* 1000 baht */}
+                            {advertise.isApprove === "Completed" ? advertise.cost + " baht"
+                                                                                : (advertise.isApprove === "In_progress" ? advertise.cost+" baht" 
+                                                                                : (advertise.isApprove === "Rejected" ? "0 baht" : "other status."))}
+                          </Td>
+                        </Tr>
+                      </Tbody>
+                    </Table>
+                  </TableContainer>
+                  <Text marginTop={2} textAlign={"right"}>
+                               {advertise.isApprove === "Completed" ? "Total cost: "+advertise.cost + " baht"
+                              : (advertise.isApprove === "In_progress" ? "Total cost: "+advertise.cost+" baht" 
+                              : (advertise.isApprove === "Rejected"))}
+                  </Text>
+                  <Text marginTop={5} textAlign={"center"}>
+                    {advertise.isApprove === "Completed" ? <ButtonComponent text="Pay now" />
+                    : (advertise.isApprove === "In_progress" ? <ButtonComponent text="Pay now" />
+                    : (advertise.isApprove === "Rejected" ))}
+                  </Text>
+                </Box>
+              </Stack>
+            </CardBody>
+          </Card>
+        ))}
       </Box>
     </Center>
   );
