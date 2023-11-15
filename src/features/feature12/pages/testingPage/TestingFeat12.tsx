999eb01dcd53455e3624e995010d08aba640af67
Vincent-NHtun <naylinhtunclifton1466@gmail.com>
2023-11-15 18:20:03 +0700

    [ADD] socket error checking
    1) added testing page for Feature12 along with css file and route for that.

---
+++ src/features/feature12/pages/testingPage/TestingFeat12.tsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react";
+import * as io from "socket.io-client";
+//import css
+import "./index.css";
+
+export const TestingFeat12 = () => {
+  //  interface ServerToClientEvents {
+  //   serverMsg: (data: { msg: string; room: string }) => void;
+  // }
+
+  //  interface ClientToServerEvents {
+  //   clientMsg: (data: { msg: string; room: string }) => void;
+  // }
+ const socket = io.connect("http://localhost:8080");
+  
+  const [msg, setMsg] = useState("");
+  const [messages, setMessages] = useState<string[]>([]);
+  const [room, setRoom] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    socket.emit("clientMsg", { msg, room });
+    setMsg("");
+  };
+
+  useEffect(() => {
+    socket.on("serverMsg", (data) => {
+      setMessages([...messages!, data.msg]);
+    });
+  }, [socket, messages]);
+  console.log(messages);
+
+  return (
+    <div className="App">
+      <div>
+        <h1>Messages</h1>
+        {messages.map((msg, i) => (
+          <p key={i} className="msg">
+            {msg}
+          </p>
+        ))}
+      </div>
+      <form onSubmit={handleSubmit} className="user-interface">
+        <input
+          value={room}
+          onChange={(e) => setRoom(e.target.value)}
+          type="text"
+          placeholder="Enter Room Key"
+          style={{ color: "black" }}
+        />
+
+        <input
+          value={msg}
+          onChange={(e) => setMsg(e.target.value)}
+          type="text"
+          placeholder="Enter message"
+          style={{ color: "black" }}
+        />
+
+        <button>Send Message</button>
+      </form>
+    </div>
+  );
+};
