996745157aca4a32b39f257d8590bbc1ad8011ed
3hiroha <97663526+3hiroha@users.noreply.github.com>
2023-11-15 13:52:59 +0700

    [Edit] Notification integrate
    -plz help on date is invalid date

--- src/features/feature08/pages/Notification (Business side)/Notification.tsx
+++ src/features/feature08/pages/Notification (Business side)/Notification.tsx
@@ -1,119 +1,71 @@
-import {Box, Flex, Text} from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
+type Notification = {
+  notificationId: string;
+  title: string;
+  message: string;
+  sendOn: string; // Keep sendOn as string for now
+  transactionId: string;
+};
+
+const formatDate = (dateString: string) => {
+  if (!dateString) {
+    return "Invalid Date";
+  }
+
+  const date = new Date(dateString);
+
+  if (isNaN(date.getTime())) {
+    return "Invalid Date";
+  }
+
+  const options: Intl.DateTimeFormatOptions = { year: "numeric", month: "long", day: "numeric" };
+  return date.toLocaleDateString(undefined, options);
+};
+
 export const Notification = () => {
-    return (
-        <Link to="">
-      <Flex
-        bg={"blackAlpha.300" }
-        h={"75px"}
-        align={"center"}
-        borderRadius={"10px"}
-        transition={"background-color 0.3s ease-in-out"}
-        _hover={{ bg: "blackAlpha.400" }}
-        _active={{ bg: "blackAlpha.200" }}
-        marginBottom={"10px"}
-      >
-        <Link to={"/Notification/NewReservation"}>
-          <Box ml='3'>
-            <Text fontWeight='bold'>
-              New Reservation 
-            </Text>
-            <Text fontSize='sm'>
-              Report to business
-            </Text>
-            <Text fontSize='md'> 12 hr ago</Text>
-          </Box>
+  const [notificationData, setNotificationData] = useState<Notification[]>([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8080/feature8/notifications`)
+      .then((response) => {
+        console.log("API Response:", response.data);
+        setNotificationData(response.data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  
+
+
+
+  return (
+    <div>
+      {notificationData.map((notification) => (
+        <Link key={notification.notificationId} to="">
+          <Flex
+            bg={"blackAlpha.300"}
+            h={"75px"}
+            align={"center"}
+            borderRadius={"10px"}
+            transition={"background-color 0.3s ease-in-out"}
+            _hover={{ bg: "blackAlpha.400" }}
+            _active={{ bg: "blackAlpha.200" }}
+            marginBottom={"10px"}
+          >
+            <Box ml="3">
+              <Text fontWeight="bold">{notification.title}</Text>
+              <Text fontSize="sm">{notification.message}</Text>
+              <Text fontSize="md">{formatDate(notification.sendOn)}</Text>
+              
+            </Box>
+          </Flex>
         </Link>
-      </Flex>
-      <Flex
-        bg={"blackAlpha.300" }
-        h={"75px"}
-        align={"center"}
-        borderRadius={"10px"}
-        transition={"background-color 0.3s ease-in-out"}
-        _hover={{ bg: "blackAlpha.400" }}
-        _active={{ bg: "blackAlpha.200" }}
-        marginBottom={"10px"}
-      >
-        <Link to={"/Notification/OrderUpdate"}>
-          <Box ml='3'>
-            <Text fontWeight='bold'>
-              Order update
-            </Text>
-            <Text fontSize='sm'>
-              Table no. 7
-            </Text>
-            <Text fontSize='md'> 12 hr ago</Text>
-          </Box>
-        </Link>
-      </Flex>
-      <Flex
-        bg={"blackAlpha.300" }
-        h={"75px"}
-        align={"center"}
-        borderRadius={"10px"}
-        transition={"background-color 0.3s ease-in-out"}
-        _hover={{ bg: "blackAlpha.400" }}
-        _active={{ bg: "blackAlpha.200" }}
-        marginBottom={"10px"}
-      >
-        <Link to={"/Notification/Checkout"}>
-          <Box ml='3'>
-            <Text fontWeight='bold'>
-              Checkout
-            </Text>
-            <Text fontSize='sm'>
-              Table no. 9
-            </Text>
-            <Text fontSize='md'> 12 hr ago</Text>
-          </Box>
-        </Link>
-      </Flex>
-      <Flex
-        bg={"blackAlpha.300" }
-        h={"75px"}
-        align={"center"}
-        borderRadius={"10px"}
-        transition={"background-color 0.3s ease-in-out"}
-        _hover={{ bg: "blackAlpha.400" }}
-        _active={{ bg: "blackAlpha.200" }}
-        marginBottom={"10px"}
-      >
-        <Link to={"/Notification/Update"}>
-          <Box ml='3'>
-            <Text fontWeight='bold'>
-              Update
-            </Text>
-            <Text fontSize='sm'>
-              Report to business
-            </Text>
-            <Text fontSize='md'> 12 hr ago</Text>
-          </Box>
-        </Link>
-      </Flex>
-      <Flex
-        bg={"blackAlpha.300" }
-        h={"75px"}
-        align={"center"}
-        borderRadius={"10px"}
-        transition={"background-color 0.3s ease-in-out"}
-        _hover={{ bg: "blackAlpha.400" }}
-        _active={{ bg: "blackAlpha.200" }}
-        marginBottom={"10px"}
-      >
-        <Link to={"/Notification/Advertisement"}>
-          <Box ml='3'>
-            <Text fontWeight='bold'>
-              Advertisement approved
-            </Text>
-            <Text fontSize='sm'>
-              Report to business
-            </Text>
-            <Text fontSize='md'> 12 hr ago</Text>
-          </Box>
-        </Link>
-      </Flex>
-    </Link>
-    )
-}
+      ))}
+    </div>
+  );
+};
