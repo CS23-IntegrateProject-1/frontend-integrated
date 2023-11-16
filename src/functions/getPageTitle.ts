6b28fa3aefc3006aa3dd80990e41da49ac4fd29d
Chanakarn Limprasertsiri <minklim47@gmail.com>
2023-11-16 22:50:43 +0700

    [EDIT] add title

--- src/functions/getPageTitle.ts
+++ src/functions/getPageTitle.ts
@@ -9,11 +9,16 @@ export const getPageTitle = (pathname: string) => {
       return "Account & Security";
     case "/setting/account/profile":
       return "Profile";
-
     //feature 2
+    
 
     //feature 3
-
+    case "/Restaurants":
+      return "Restaurant List"
+    case "/Clubs":
+      return "Club List"
+    case "/Bars":
+      return "Bar List"
     //feature 4
 
     //feature 5
@@ -27,6 +32,13 @@ export const getPageTitle = (pathname: string) => {
       return "Membership";
     case "/membership/my-privileges":
       return "Privileges";
+
+    //feature 10
+    //feature 11
+    case "/article":
+      return "Article"
+    //feature 12
+    //feature 13
     // more later...
     default:
       return "Harmoni";
