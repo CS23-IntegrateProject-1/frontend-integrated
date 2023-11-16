638a61ffe0940a1781fc651dc040c95cf07b7b75
Chanakarn Limprasertsiri <minklim47@gmail.com>
2023-11-16 20:35:52 +0700

    Revert "Merge pull request #39 from CS23-IntegrateProject-1/feature-04"
    This reverts commit e65522d49e4e520c01af251fb67b7c0f1aca2bbf, reversing
    changes made to 28cfd5b3f4462b666a68f9554bb910eaf92b307d.

--- src/features/feature04/Feature04Routes.tsx
+++ src/features/feature04/Feature04Routes.tsx
@@ -1,7 +1,3 @@
-import { Maps } from "./pages/Restaurants";
-import {SavedLocation} from "./pages/SavedLocation"
-import { Cinemas } from "./pages/Cinemas";
-import { Bars } from "./pages/Bars";
 export const Feature04Routes = () => {
-  return [{ path: "/map", element: <Maps/> },{path:"/map/savedlocation", element:<SavedLocation/>},{path:"/map/cinemas", element:<Cinemas/>},{path:"/map/bars", element:<Bars/>}];
+  return [{ path: "/map", element: "ElementPlaceHolder" }];
 };
