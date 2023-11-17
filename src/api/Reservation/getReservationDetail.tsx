9a4cf4d9e4d0c8e1104f523cecb2c34e0aedfe75
jjustmint <123088410+jjustmint@users.noreply.github.com>
2023-11-17 14:08:29 +0700

    [EDIT] getReservationDetail

--- src/api/Reservation/getReservationDetail.tsx
+++ src/api/Reservation/getReservationDetail.tsx
@@ -1,7 +1,7 @@
 import { Axios } from "../../AxiosInstance"
 export const getReservationDetail = async(userId:number) => {
     try {
-        const response = await Axios.post("/MyReservation/:venueId/:reservationId");
+        const response = await Axios.get("/MyReservation/:venueId/:reservationId");
         return response.data;
     }
     catch (e) {
