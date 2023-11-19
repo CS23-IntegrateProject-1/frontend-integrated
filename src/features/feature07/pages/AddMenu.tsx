954f2d8ba04ab7c4f786422e3121aa9ec254d631
Nan <nanzunlapyae2@gmail.com>
2023-11-19 23:42:36 +0700

    [Progress] Add Menu Page

--- src/features/feature07/pages/AddMenu.tsx
+++ src/features/feature07/pages/AddMenu.tsx
@@ -1,7 +1,77 @@
-export const AddMenu = () => {
-    return (
-        <div>
-        <h1>AddMenu</h1>
-        </div>
-    );
-    };
+import React from 'react';
+import { FormControl, FormLabel, Input, Box, Center } from '@chakra-ui/react';
+
+interface AddMenuProps {}
+
+export const AddMenu: React.FC<AddMenuProps> = () => {
+  return (
+    <FormControl>
+      <Box display="flex" flexDirection="column">
+        <Center>
+          <Box>
+            <FormLabel>Food name</FormLabel>
+            <Input
+              size="sm"
+              variant="outline"
+              width="307px"
+              height="32px"
+              padding="0px 12px 0px 12px"
+              borderRadius="4px"
+              borderColor="brand.300"
+              bgColor="brand.300"
+              marginBottom="10px"
+              color="gray.300"
+            />
+          </Box>
+        </Center>
+
+        <Center>
+          <Box>
+            <FormLabel>Description</FormLabel>
+            <Input
+              variant="outline"
+              width="307px"
+              height="60px"
+              marginBottom="10px"
+              padding="0px 12px 0px 12px"
+              borderColor="brand.300"
+              bgColor="brand.300"
+            />
+          </Box>
+        </Center>
+
+        <Center>
+          <Box>
+            <FormLabel>Price</FormLabel>
+            <Input
+              size="sm"
+              variant="outline"
+              width="307px"
+              height="32px"
+              padding="0px 12px 0px 12px"
+              borderRadius="4px"
+              borderColor="brand.300"
+              bgColor="brand.300"
+              marginBottom="10px"
+            />
+          </Box>
+        </Center>
+
+        <Center>
+          <Box>
+            <FormLabel>Upload Image</FormLabel>
+            <Input
+              variant="outline"
+              width="250px" // Adjust width as needed
+              height="130px"
+              padding="0px 12px 0px 12px"
+              marginBottom="10px"
+              borderColor="brand.300"
+              bgColor="brand.300"
+            />
+          </Box>
+        </Center>
+      </Box>
+    </FormControl>
+  );
+};
