5c3a45fe54e1ebce5b96bc577cd185032d798433
Chanakarn Limprasertsiri <minklim47@gmail.com>
2023-11-21 22:05:45 +0700

    [EDIT] modify image preview in advertise/request

--- src/features/feature05/pages/businessAdvertisement/AdvertisementRequestPage.tsx
+++ src/features/feature05/pages/businessAdvertisement/AdvertisementRequestPage.tsx
@@ -25,7 +25,7 @@ import { ChangeEvent, useEffect, useState } from "react";
 import { BiImageAdd } from "react-icons/bi";
 import { AiOutlineClose } from "react-icons/ai";
 import { Axios } from "../../../../AxiosInstance";
-  
+
 interface AdvertisementProps {
   name: string;
   description: string;
@@ -38,7 +38,7 @@ interface AdvertisementProps {
 }
 export const AdvertisementRequestPage = () => {
   const navigate = useNavigate();
-  const { isOpen, onOpen, onClose } = useDisclosure()
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const [file, setFile] = useState<File | null>(null);
   const [imagePreview, setImagePreview] = useState<string | null>(null);
   const [advertise, setAdvertise] = useState<AdvertisementProps>({
@@ -180,66 +180,83 @@ export const AdvertisementRequestPage = () => {
       </FormControl>
 
       {/* Image */}
-      <FormControl
-        isRequired
-        width="50%"
-        minWidth="250px"
-        maxWidth="400px"
-        display="flex"
-        flexDirection={"column"}
-        paddingBottom={3}
-      >
-        <FormLabel style={TextStyle.h2} color={"white"} paddingBottom={1}>
-          {" "}
-          Images
-        </FormLabel>
-        <Stack spacing={2} direction="column">
-          <Center
-            width={"auto"}
-            height={"100"}
-            bg={"#5F0DBB"}
-            borderRadius={5}
-            cursor={"pointer"}
-          >
-            <Input
-              onChange={handleFileChange}
-              type="file"
-              opacity={0}
-              height={"100%"}
-              w={"100%"}
-              pos={"absolute"}
-            ></Input>
-            <Icon
-              as={BiImageAdd}
-              color={"#FFFFFF"}
-              width={"auto"}
-              height={"8"}
-            ></Icon>
-          </Center>
-        </Stack>
-      </FormControl>
+
       {imagePreview ? (
-        <Box
-          position={"relative"}
-          overflow={"hidden"}
-          minWidth={"50%"}
-          maxWidth={"50%"}
-          height={"auto"}
+        <FormControl
+          isRequired
+          width="50%"
+          minWidth="250px"
+          maxWidth="400px"
+          display="flex"
+          flexDirection={"column"}
+          paddingBottom={3}
         >
-          <IconButton
-            aria-label="close"
-            minWidth={"15px"}
-            height={"15px"}
-            position={"absolute"}
-            top={0}
-            right={0}
-            as={AiOutlineClose}
-            onClick={handleCloseImage}
-          ></IconButton>
-          <Image src={imagePreview} alt={"image"} width={"100%"}></Image>
-        </Box>
+          <FormLabel style={TextStyle.h2} color={"white"}>
+            Image
+          </FormLabel>
+
+          <Box
+            position={"relative"}
+            overflow={"hidden"}
+            width={"100%"}
+            minWidth={"250px"}
+            maxWidth={"400px"}
+            height={"auto"}
+            alignSelf={"center"}
+          >
+            <IconButton
+              aria-label="close"
+              minWidth={"15px"}
+              height={"15px"}
+              position={"absolute"}
+              top={0}
+              right={0}
+              as={AiOutlineClose}
+              onClick={handleCloseImage}
+            ></IconButton>
+            <Image src={imagePreview} alt={"image"} width={"100%"}></Image>
+          </Box>
+        </FormControl>
       ) : (
-        <></>
+        <FormControl
+          isRequired
+          width="50%"
+          minWidth="250px"
+          maxWidth="400px"
+          display="flex"
+          flexDirection={"column"}
+          paddingBottom={3}
+        >
+          <FormLabel style={TextStyle.h2} color={"white"} paddingBottom={1}>
+            {" "}
+            Image
+          </FormLabel>
+          <Stack spacing={2} direction="column">
+            {}
+            <Center
+              width={"auto"}
+              height={"100"}
+              bg={"#5F0DBB"}
+              borderRadius={5}
+              cursor={"pointer"}
+            >
+              <Input
+                onChange={handleFileChange}
+                type="file"
+                opacity={0}
+                height={"100%"}
+                w={"100%"}
+                pos={"absolute"}
+              ></Input>
+              <Icon
+                as={BiImageAdd}
+                color={"#FFFFFF"}
+                width={"auto"}
+                height={"8"}
+              ></Icon>
+            </Center>
+          </Stack>
+        </FormControl>
       )}
 
       {/* Target customer */}
@@ -328,18 +345,32 @@ export const AdvertisementRequestPage = () => {
           Submit
         </Button>
         <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent bgColor={"#DEBEF6"} color={"#200944"} >
-          <ModalHeader mt={3}>Submit advertisement</ModalHeader>
-          <ModalCloseButton />
-          <ModalFooter>
-            <Button bgColor={"white"} color={"#200944"} mr={5} width="30%" onClick={onClose} >Cancel</Button>
-            <Button bgColor={"#A533C8"} mr={3} onClick={handleClick} color={"white"} width="30%">
-              Confirm
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+          <ModalOverlay />
+          <ModalContent bgColor={"#DEBEF6"} color={"#200944"}>
+            <ModalHeader mt={3}>Submit advertisement</ModalHeader>
+            <ModalCloseButton />
+            <ModalFooter>
+              <Button
+                bgColor={"white"}
+                color={"#200944"}
+                mr={5}
+                width="30%"
+                onClick={onClose}
+              >
+                Cancel
+              </Button>
+              <Button
+                bgColor={"#A533C8"}
+                mr={3}
+                onClick={handleClick}
+                color={"white"}
+                width="30%"
+              >
+                Confirm
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
       </Box>
     </Box>
   );
