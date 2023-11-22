229fe52d554a65bcd2c8b0c03385880abfa012ed
jjustmint <123088410+jjustmint@users.noreply.github.com>
2023-11-23 00:42:47 +0700

    Fix missing files

--- src/features/feature06/pages/MyReservation.tsx
+++ src/features/feature06/pages/MyReservation.tsx
@@ -1,6 +1,6 @@
 import { Box, Heading, Text, Button, Card } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
-import { ReservationCards } from "../components/ReservationCards";  
+import { ReservationCards } from "../components/ReservationCards";
 import { getMyReservation } from "../../../api/Reservation/getMyReservation";
 import { ButtonMyReservation } from "../components/ButtonMyReservation";
 
@@ -39,7 +39,7 @@ export const MyReservation = () => {
   const [data, setData] = useState<IData[]>([]);
   useEffect(() => {
     fetchData();
-  }, []); 
+  }, []);
 
   const fetchData = async () => {
     const response: IData[] = await getMyReservation(1, status);
@@ -86,10 +86,22 @@ export const MyReservation = () => {
         justifyContent={"flex-start"}
         position={"relative"}
       >
-        <ButtonMyReservation onClick={()=>setStatus("Pending")} text="Pending"></ButtonMyReservation>
-        <ButtonMyReservation onClick={()=>setStatus("Check_in")} text="Check_in"></ButtonMyReservation>
-        <ButtonMyReservation onClick={()=>setStatus("Check_out")} text="Check_out"></ButtonMyReservation>
-        <ButtonMyReservation onClick={()=>setStatus("Cancel")} text="Canceled"></ButtonMyReservation>
+        <ButtonMyReservation
+          onClick={() => setStatus("Pending")}
+          text="Pending"
+        ></ButtonMyReservation>
+        <ButtonMyReservation
+          onClick={() => setStatus("Check_in")}
+          text="Check_in"
+        ></ButtonMyReservation>
+        <ButtonMyReservation
+          onClick={() => setStatus("Check_out")}
+          text="Check_out"
+        ></ButtonMyReservation>
+        <ButtonMyReservation
+          onClick={() => setStatus("Cancel")}
+          text="Canceled"
+        ></ButtonMyReservation>
       </Box>
       <Box className="ReservationList" marginTop={"10px"}>
         {renderCards()}
