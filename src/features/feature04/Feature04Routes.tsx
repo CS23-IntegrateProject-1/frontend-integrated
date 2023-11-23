001bcbcbbc85c7f1e30360ba13fbaba9d9371dae
ntck <nontakorn.chatk@gmail.com>
2023-11-23 22:50:56 +0700

    food-delivery(on progress)

--- src/features/feature04/Feature04Routes.tsx
+++ src/features/feature04/Feature04Routes.tsx
@@ -2,11 +2,19 @@ import { Maps } from "./pages/Restaurants";
 import { SavedLocation } from "./pages/SavedLocation";
 import { Cinemas } from "./pages/Cinemas";
 import { Bars } from "./pages/Bars";
+import { CartMenuDetail } from "./pages/CartMenuDetail";
+import FoodDelivery from "./pages/FoodDelivery";
+
 export const Feature04Routes = () => {
   return [
     { path: "/map", element: <Maps /> },
     { path: "/map/savedlocation", element: <SavedLocation /> },
     { path: "/map/cinemas", element: <Cinemas /> },
     { path: "/map/bars", element: <Bars /> },
+    { path: "/map/bars", element: <Bars /> },
+    {path:"/map/food-delivery", element:<FoodDelivery/>},
+    {path:"/map/food-delivery/food-detail", element:<CartMenuDetail/>},
+
+
   ];
 };
---
+++ src/features/feature04/pages/FoodDelivery.tsx
@@ -0,0 +1,63 @@
+import { Box, Button, Divider, Flex, IconButton, Text } from "@chakra-ui/react";
+import { MenuComp } from "../components/FoodDeliveryComp/MenuComp";
+import { FaShoppingCart } from "react-icons/fa";
+import { FoodDeliNavbar } from "../components/FoodDeliveryComp/FoodDeliNavbar";
+import index from "../../../theme/foundations/index";
+const FoodDelivery = () => {
+  return (
+    <Box>
+      <FoodDeliNavbar RestaurantName="MK Restaurant (Big C Rama 4)" DeliveryMinute={30}/>
+      <Flex flexDir={"column"} alignItems="center">
+        <Flex flexWrap="wrap" justifyContent="center" maxW="800px" gap={5}>
+          <MenuComp menuName="MK Roasted Duck" price={210.0}/>
+          <MenuComp menuName="MK Roasted Duck"price={210.0} />
+          <MenuComp menuName="MK Roasted Duck" price={210.0}/>
+          <MenuComp menuName="MK Roasted Duck" price={210.0}/>
+          <MenuComp menuName="MK Roasted Duck"price={210.0} />
+          <MenuComp menuName="MK Roasted Duck"price={210.0} />
+        </Flex>
+      </Flex>
+      
+      <Flex justifyContent={"center"}>
+        <Button
+          variant={"unstyle"}
+          display={"flex"}
+          justifyContent={"space-between"}
+          width={"auto"}
+          minW={"300"}
+          height={70}
+          background={index.colors.brand[200]}
+          color={index.colors.white}
+          m={10}
+        >
+          <Text
+            borderRadius={"100%"}
+            borderWidth="2px"
+            borderStyle="solid"
+            width={"2em"}
+            height={"2em"}
+            textAlign={"center"}
+            display={"flex"}
+            justifyContent={"center"}
+            alignItems={"center"}
+          >
+            1
+          </Text>
+          <Text
+            fontSize={index.textStyles.body1.fontSize}
+            fontWeight={index.textStyles.h1.fontWeight}
+          >
+            View Your Cart
+          </Text>
+          <Text
+            fontSize={index.textStyles.body1.fontSize}
+            fontWeight={index.textStyles.body1.fontWeight}
+          >
+            $210.0
+          </Text>
+        </Button>
+      </Flex> 
+    </Box>
+  );
+};
+export default FoodDelivery;
