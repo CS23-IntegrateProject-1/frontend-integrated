cd4c3f9bfac09af7ffc72b6bae63d6cb2431e77d
Snailoba <fameeverywhere@gmail.com>
2023-11-23 22:25:26 +0700

    [ADD] Review writing pages
    -Review writing page for delivery
    -Review writing page for reservation
    -Dedicated routes for the new pages
    -Adjusted minor detail

--- src/features/feature03/Feature03Routes.tsx
+++ src/features/feature03/Feature03Routes.tsx
@@ -5,6 +5,8 @@ import { RestaurantPage } from "./pages/homePage/RestaurantPage";
 import { ReviewPage } from "./pages/homePage/ReviewPage";
 import { VenueDetail } from "./pages/homePage/VenueDetail";
 import { VenueBranches } from "./pages/homePage/VenueBranches";
+import { ReviewDelivery } from "./pages/homePage/F3_RVPCs/ReviewDelivery"
+import { ReviewReservation } from "./pages/homePage/F3_RVPCs/ReviewReservation"
 
 export const Feature03Routes = () => {
   return [
@@ -15,6 +17,8 @@ export const Feature03Routes = () => {
     { path: "/Promotions", element: <PromotionsPage /> },
     { path: "/venue-list", element: "ElementPlaceHolder" },
     { path: "/venuevenue-branch/:branchId", element: <VenueDetail /> },
-    { path: "/Branches/:venueId", element: <VenueBranches />}
+    { path: "/Branches/:venueId", element: <VenueBranches /> },
+    { path: "/ReviewDel", element: <ReviewDelivery /> },
+    { path: "/ReviewRes", element: <ReviewReservation /> }
   ];
 };
