98dca46c50a5860f91c0e11ca13a4c01c07e2052
jjustmint <123088410+jjustmint@users.noreply.github.com>
2023-11-24 02:05:50 +0700

    delete Business Routes from Routes

--- src/features/feature06/Feature06Routes.tsx
+++ src/features/feature06/Feature06Routes.tsx
@@ -2,12 +2,6 @@ import { MyReservation } from "./pages/MyReservation";
 import { ReservationDetail } from "./pages/ReservationDetail";
 import { TablePage } from "./pages/TablePage";
 import { TableType } from "./pages/TableType";
-import { Dashboard } from "./pages/Dashboard";
-import { ViewTable } from "./pages/ViewTable";
-import { Reservation } from "./pages/Business/Reservation";
-import { WalkInPeople } from "./pages/Business/WalkInPeople";
-import { WalkInDetail } from "./pages/Business/WalkInDetail";
-import { TableList } from "./pages/Business/TableList";
 import { GetReservationDetail } from "./pages/GetReservationDetail";
 import { CreateTable1 } from "./pages/CreateTable1";
 import { CreateTable2 } from "./pages/CreateTable2";
@@ -23,15 +17,8 @@ export const Feature06Routes = () => {
     { path: "/my-reservation", element: <MyReservation /> },
     { path: "/table", element: <TablePage /> },
     { path: "/tabletype", element: <TableType /> },
-    { path: "/dashboard", element: <Dashboard /> },
+    // { path: "/dashboard", element: <Dashboard /> },
     { path: "/reservation-detail", element: <ReservationDetail /> },
-    { path: "/Reservation", element: <Reservation /> },
-    { path: "/WalkInDetail", element: <WalkInDetail /> },
-    { path: "/WalkInPeople", element: <WalkInPeople /> },
-    { path: "/viewtable", element: <ViewTable /> },
-    { path: "/tablelist", element: <TableList /> },
     { path: "/getreservation-detail", element: <GetReservationDetail /> },
-    { path: "/createtable1", element: <CreateTable1 /> },
-    { path: "/createtable2", element: <CreateTable2 /> },
   ];
 };
