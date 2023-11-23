c25c71e521e162f48b800c5783436d87fa025aa7
mmarkch <mark110447@gmail.com>
2023-11-23 20:59:18 +0700

    [ADD] Payment Page and Payment Success Page

--- src/features/feature10/Feature10Routes.tsx
+++ src/features/feature10/Feature10Routes.tsx
@@ -1,6 +1,8 @@
 import CinemaDetailPage from "./pages/CinemaDetails";
 import { CinemaMain } from "./pages/CinemaMain";
+import PaymentPage from "./pages/Payment";
 import { ShowTime } from "./pages/ShowTime";
+import PaymentSuccess from "./pages/PaymentSuccess";
 export const Feature10Routes = () => {
   return [
     {
@@ -12,7 +14,21 @@ export const Feature10Routes = () => {
       element: <ShowTime />,
 
     },
-    { path: "/event/:eventId", element: "ElementPlaceHolder" },
-    { path: "/cinemaDetails", element: <CinemaDetailPage /> },
+    { 
+      path: "/event/:eventId", 
+      element: "ElementPlaceHolder" 
+    },
+    { 
+      path: "/cinemaDetails", 
+      element: <CinemaDetailPage /> 
+    },
+    {
+      path: "/payment",
+      element: <PaymentPage />
+    },
+    {
+      path: "/paymentSuccess",
+      element: <PaymentSuccess />
+    }
   ];
 };
