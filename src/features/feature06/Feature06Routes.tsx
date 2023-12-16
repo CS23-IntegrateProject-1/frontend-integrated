80082a35703f075d59c8f4fadd7e94a9e2c03917
Thawatchai Wongboonsiri <jj.wbsr@gmail.com>
2023-12-16 22:24:51 +0700

    [UPDATE] routes, RDQrcode

--- src/features/feature06/Feature06Routes.tsx
+++ src/features/feature06/Feature06Routes.tsx
@@ -1,11 +1,12 @@
 import { MyReservation } from "./pages/MyReservation";
-import { ReservationDetail } from "./pages/ReservationDetail";
+import { ReservationDetailConfirm } from "./pages/RDConfirm";
 import { TablePage } from "./pages/TablePage";
 import { TableType } from "./pages/TableType";
 import { GetReservationDetail } from "./pages/GetReservationDetail";
 import { CreateTable1 } from "./pages/CreateTable1";
 import { CreateTable2 } from "./pages/CreateTable2";
 import { QrcodeConfirm } from "./pages/QrcodeConfirm";
+import { ReservationDetailQrcode } from "./pages/RDQrcode";
 
 export const Feature06Routes = () => {
   return [
@@ -19,8 +20,11 @@ export const Feature06Routes = () => {
     { path: "/table", element: <TablePage /> },
     { path: "/tabletype", element: <TableType /> },
     // { path: "/dashboard", element: <Dashboard /> },
-    { path: "/reservation-detail", element: <ReservationDetail /> },
+    { path: "/reservation-detail-confirm", element: <ReservationDetailConfirm /> },
+    { path: "/reservation-detail-qrcode", element: <ReservationDetailQrcode /> },
     { path: "/getreservation-detail", element: <GetReservationDetail /> },
+    { path: "/createtable1", element: <CreateTable1 /> },
+
     
   ];
 };
