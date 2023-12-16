cf0c1adfec67df5ac23abafcfe2582a7ce4b71de
NayCMMKyaw <nayche.moh1@kmutt.ac.th>
2023-12-16 18:49:21 +0700

    [EDIT] Integrate Receipt

--- src/features/feature07/pages/ReceiptPage.tsx
+++ src/features/feature07/pages/ReceiptPage.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Box, Text ,HStack, VStack,Flex} from '@chakra-ui/react';
 import textStyles from '../../../theme/foundations/textStyles';
 import {ButtonComponent} from '../../../components/buttons/ButtonComponent';
+import { Axios } from '../../../AxiosInstance';
+import { useQuery } from '@tanstack/react-query';
+import { formatDate1, formatTime1 } from '../../../functions/formatDatetime';
 
 interface ReceiptPageProps {
   orderId: string;
@@ -10,36 +13,52 @@ interface ReceiptPageProps {
 }
 
 export const ReceiptPage: React.FC<ReceiptPageProps> = ({ orderId, orderDate, itemCount }) => {
-    const itemsPerRow = 3;
+
+  const { data: receipt } = useQuery(['orderReceipt'], async () => {
+    const response = await Axios.get('/feature7/getReceipt');
+    console.log("Receipt data:" ,response.data);
+    return response.data;
+  });
+
   return (
     <Box>
-      <Text {...textStyles.h2}>Order #intergrate</Text>
-      <Text {...textStyles.h3}>order date</Text>
+      {/* {receipt && () } */}
+      <Text {...textStyles.h2}>Order #{receipt?.orderId}</Text>
+      <Text {...textStyles.h3}>{formatDate1(receipt?.orderDate)} {formatTime1(receipt?.orderDate)}</Text>
       <Box borderTop="1px dotted" mt={2} mb={2}/>
             <HStack display="flex" justifyContent="space-between">
-            <Text>QTY</Text>
-            <Text>ITEM</Text>
-            <Text>AMT</Text>
+              <Text>QTY</Text>
+              <Text>ITEM</Text>
+              <Text>PRICE</Text>
+              <Text>AMT</Text>
             </HStack>
         <Box borderTop="1px dotted" mt={2} mb={2} />
         <VStack spacing={2} align="start">
-        {[...Array(itemCount)].map((_, index) => (
+        {receipt?.orderDetails?.map((order, index) => (
           <Box key={index} width="100%">
-                <HStack justifyContent="space-between">
-                <Text>{(index + 1).toString().padStart(2, '0')}</Text>
-                <Text>Item Name</Text>
-                <Text>Amount</Text>
+                <HStack display="flex" justifyContent="space-between">
+                  <Text>{(index + 1).toString().padStart(2, '0')}</Text>
+                  <Text>{order.menuName}</Text>
+                  <Text>PRICE</Text>
+                  <Text>Amount</Text>
                 </HStack>
           </Box>
         ))}
         </VStack>
          <Box borderTop="1px dotted" mt={2} mb={2}/>
         
+        <HStack display="flex" justifyContent="space-between">
         <VStack align="start" spacing={0}>
             <Text {...textStyles.h3}>ITEM COUNT :</Text>
-            <Text {...textStyles.h3}>TOTAL :</Text>
-            <Text {...textStyles.h3}>PRICE :</Text>
+            <Text {...textStyles.h3}>TOTAL COUNT :</Text>
+            <Text {...textStyles.h3}>TOTAL AMOUNT :</Text>
         </VStack>
+        <VStack align="end" spacing={0}>
+            <Text {...textStyles.h3}>{receipt?.itemCount}</Text>
+            <Text {...textStyles.h3}>{receipt?.totalCount}</Text>
+            <Text {...textStyles.h3}>{receipt?.totalAmount}</Text>
+        </VStack>
+        </HStack>
         <Box borderTop="1px dotted" mt={2} mb={2} />
         <Flex align="center" justify="center" >
         <Box
