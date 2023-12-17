e1860fc7f3630b2184e4ff18ac420846f8441d30
3hiroha <97663526+3hiroha@users.noreply.github.com>
2023-12-18 00:52:56 +0700

    Edit Receipt

--- src/features/feature08/pages/Admin/Receipt.tsx
+++ src/features/feature08/pages/Admin/Receipt.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Flex, Box, Text, Spacer } from '@chakra-ui/react';
 import { useLocation, useParams } from 'react-router-dom';
 import { Axios } from '../../../../AxiosInstance';
