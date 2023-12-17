6ba7bcb6c0f9f766b69076ddc2e98e70b6c46b5f
Kaung101 <khin.tun1@mail.kmutt.ac.th>
2023-12-17 19:17:20 +0700

    [FIX] error
    fix minor error

--- src/routes/PrivateBusinessRoutes.tsx
+++ src/routes/PrivateBusinessRoutes.tsx
@@ -2,15 +2,17 @@ import { Navigate, createBrowserRouter } from "react-router-dom";
 import { RootLayout } from "../layouts/RootLayout";
 import { NotFoundPage } from "../pages/fallbackPages/NotFoundPage";
 import { Feature13Routes } from "../features/feature13/Feature13Routes";
+
 import { Feature13PublicRoutes } from "../features/feature13/Feature13PublicRoutes";
 import { Feature06BusinessPrivateRoutes } from "../features/feature06/Feature06BusinessPrivateRoutes";
+import { Feature01PrivateRoutes } from "../features/feature01/Feature01PrivateRoutes";
 
 export const PrivateBusinessRoutes = createBrowserRouter([
 	{ path: "/", element: <Navigate to={"/business/dashboard"} /> },
 	{
 		path: "/",
 		element: <RootLayout role="business" />,
-		children: [...Feature13Routes(), ...Feature13PublicRoutes(), ...Feature06BusinessPrivateRoutes()],
+		children: [...Feature13Routes(), ...Feature13PublicRoutes(), ...Feature06BusinessPrivateRoutes(),...Feature01PrivateRoutes() ],
 	},
 	{ path: "/*", element: <NotFoundPage /> },
 ]);
