89dbfbfe69ce3d7e8085224f3603d10f04c1c0b1
Chayapol Mahatthanachai <best37@hotmail.com>
2023-12-17 20:50:54 +0700

    [UPDATE] Select Payment
    Update UI of Checkout button

--- src/features/feature08/pages/Customer/SelectPayment.tsx
+++ src/features/feature08/pages/Customer/SelectPayment.tsx
@@ -1,19 +1,21 @@
 import { QrCodeButton } from "../QrCode/QrCodeButton";
-import { MobileBankingList } from "../MobileBanking/MobileBankingList"
+import { MobileBankingList } from "../MobileBanking/MobileBankingList";
 import { CreditCardList } from "../CreditCard/CreditCardList";
 import { ConfirmButton } from "../Confirm/ConfirmButton";
-import { Box, Button,} from "@chakra-ui/react";
-import {MdAttachMoney} from "react-icons/md"
+import { Box, Button } from "@chakra-ui/react";
+import { MdAttachMoney } from "react-icons/md";
 import { FC, useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import PayButton from "./PayButton";
 
-import { loadStripe } from '@stripe/stripe-js';
+import { loadStripe } from "@stripe/stripe-js";
 import { Axios } from "../../../../AxiosInstance";
 
-const stripePromise = loadStripe('pk_test_51OFf98BCLtNTpQNyKo7pOR2Oyl2N3LxLtvGO549ogZUwpqgAUY0ycFgCYGhJbNXXnnyy1eLxTC2czmCuZqRd5BKy00lHA8sWfw'); // replace 'your_publishable_key' with your actual publishable key
+const stripePromise = loadStripe(
+  "pk_test_51OFf98BCLtNTpQNyKo7pOR2Oyl2N3LxLtvGO549ogZUwpqgAUY0ycFgCYGhJbNXXnnyy1eLxTC2czmCuZqRd5BKy00lHA8sWfw"
+); // replace 'your_publishable_key' with your actual publishable key
 
 interface ButtonProps {
   bgColor?: string;
@@ -23,17 +25,16 @@ interface ButtonProps {
 }
 
 type creditCardUser = {
-  creditCardId:string;
-  card_no:string;
-  name:string;
-  country:string;
-  bank:string;
-  cvc:string;
-  exp:Date;
-  userId:string;
-
-}
-export const SelectPayment: FC<ButtonProps>= ({
+  creditCardId: string;
+  card_no: string;
+  name: string;
+  country: string;
+  bank: string;
+  cvc: string;
+  exp: Date;
+  userId: string;
+};
+export const SelectPayment: FC<ButtonProps> = ({
   bgColor,
   textColor,
   borderColor,
@@ -44,7 +45,6 @@ export const SelectPayment: FC<ButtonProps>= ({
 
   const redirectToCheckout = async (event: React.FormEvent) => {
     event.preventDefault();
-    
 
     // const stripe = await stripePromise;
 
@@ -53,23 +53,24 @@ export const SelectPayment: FC<ButtonProps>= ({
     //   method: "POST",
     // });
 
-    
-    Axios.post('/feature8/create-checkout-session',{} ).then(async (res)=> {
-      // const session = res;
-      // const result = await stripe?.redirectToCheckout({
-      //   sessionId: res.data.id,
-      // });
-  
-      // // If redirectToCheckout fails due to a browser or network error, you should display the localized error message to your customer
-      // if (result.error) {
-      //   alert(result.error.message);
-      // }
-      console.log(res.data)
-      window.location.href = res.data.url
-    }).catch((err)=> {
-      console.log(err)
-      throw err;
-    })
+    Axios.post("/feature8/create-checkout-session", {})
+      .then(async (res) => {
+        // const session = res;
+        // const result = await stripe?.redirectToCheckout({
+        //   sessionId: res.data.id,
+        // });
+
+        // // If redirectToCheckout fails due to a browser or network error, you should display the localized error message to your customer
+        // if (result.error) {
+        //   alert(result.error.message);
+        // }
+        console.log(res.data);
+        window.location.href = res.data.url;
+      })
+      .catch((err) => {
+        console.log(err);
+        throw err;
+      });
 
     // const session = await response.json();
 
@@ -101,32 +102,31 @@ export const SelectPayment: FC<ButtonProps>= ({
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(`http://localhost:8080/feature8/creditcardU/${userId}`);
+        const response = await axios.get(
+          `http://localhost:8080/feature8/creditcardU/${userId}`
+        );
         setCreditCardUser(response.data);
-        console.log(response.data)
+        console.log(response.data);
       } catch (error) {
-        console.error('Error fetching credit card data:', error);
+        console.error("Error fetching credit card data:", error);
       }
     };
-  
+
     if (userId) {
       fetchData();
     }
   }, [userId]);
-  
 
-
-
-    return (
-        <Box
-        display={"flex"}
-        flexDirection={"column"}
-        justifyContent={"center"}
-        alignItems={"center"}
-        margin={5}
-      >
+  return (
+    <Box
+      display={"flex"}
+      flexDirection={"column"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      margin={5}
+    >
       {/* <form onSubmit={redirectToCheckout}> */}
-        {/* <Button
+      {/* <Button
               type="submit"
               id="checkout-button"
             width={"70%"}
@@ -140,16 +140,28 @@ export const SelectPayment: FC<ButtonProps>= ({
             >
             Cash
         </Button> */}
-        {/* <button type="submit">Check Out</button> */}
-                <Button onClick={redirectToCheckout}>Check Out</Button>
+      {/* <button type="submit">Check Out</button> */}
+      <Button
+        onClick={redirectToCheckout}
+        width={"80%"}
+        height={"40px"}
+        bg={!bgColor ? "brand.200" : bgColor}
+        color={!textColor ? "white" : textColor}
+        borderColor={!borderColor ? "" : borderColor}
+        _hover={{ bg: !bgHover ? "brand.300" : bgHover }}
+        textColor={"#DEBEF6"}
+        leftIcon={<MdAttachMoney />}
+      >
+        Check Out
+      </Button>
 
       {/* </form> */}
-          
-        {/* <PayButton cartItems={[]} /> */}
-        <QrCodeButton />
-        <MobileBankingList />
-        <CreditCardList card={creditCardUser} />
-        <ConfirmButton />
-        </Box>
-    )
-}
+
+      {/* <PayButton cartItems={[]} /> */}
+      <QrCodeButton />
+      <MobileBankingList />
+      <CreditCardList card={creditCardUser} />
+      <ConfirmButton />
+    </Box>
+  );
+};
