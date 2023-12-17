7eb78b32bf7ccc7367b5cdc30f3a53e59356327c
Nan <nanzunlapyae2@gmail.com>
2023-12-17 15:34:54 +0700

    [Update] Receipt Page

--- src/features/feature07/pages/ReceiptPage.tsx
+++ src/features/feature07/pages/ReceiptPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Text ,HStack, VStack,Flex} from '@chakra-ui/react';
+import { Box, Text ,HStack, VStack,Flex,Center} from '@chakra-ui/react';
 import textStyles from '../../../theme/foundations/textStyles';
 import {ButtonComponent} from '../../../components/buttons/ButtonComponent';
 import { Axios } from '../../../AxiosInstance';
@@ -7,14 +7,10 @@ import { useQuery } from '@tanstack/react-query';
 import { formatDate1, formatTime1 } from '../../../functions/formatDatetime';
 import { FullPageLoader } from '../../../components/Loader/FullPageLoader';
 
-interface ReceiptPageProps {
-  orderId: string;
-  orderDate: string;
-  itemCount: number;
-}
 
-export const ReceiptPage: React.FC<ReceiptPageProps> = () => {
 
+export const ReceiptPage = () => {
+ 
   const { data:receipt, isLoading,isError, error } = useQuery(['orderReceipt'], async () => {
     const response = await Axios.get('/feature7/getReceipt');
     console.log("Receipt data:" ,response.data);  
@@ -26,29 +22,30 @@ export const ReceiptPage: React.FC<ReceiptPageProps> = () => {
   if (isError){
     return <div>An error occurred: {(error as Error).message}</div>;
   }
-
+ 
   return (
-    <Box>
+    <Center>
+    <Box width="80%">
       {/* {receipt && () } */}
       <Text {...textStyles.h2}>Order #{receipt?.orderId}</Text>
       {/* <Text>{receipt.orderDate? receipt.orderDate :"hello"}</Text> */}
       <Text {...textStyles.h3}>{formatDate1(receipt?.orderDate)} {formatTime1(receipt?.orderDate)}</Text>
       <Box borderTop="1px dotted" mt={2} mb={2}/>
             <HStack display="flex" justifyContent="space-between">
-              <Text>QTY</Text>
-              <Text>ITEM</Text>
-              <Text>PRICE</Text>
-              <Text>AMT</Text>
+              <Text flex="3">QTY</Text>
+              <Text flex="3">ITEM</Text>
+              <Text flex="2">PRICE</Text>
+              <Text flex="2" align="end">AMT</Text>
             </HStack>
         <Box borderTop="1px dotted" mt={2} mb={2} />
         <VStack spacing={2} align="start">
         {receipt?.orderDetails?.map((order, index) => (
           <Box key={index} width="100%">
                 <HStack display="flex" justifyContent="space-between">
-                  <Text>{(index + 1).toString().padStart(2, '0')}</Text>
-                  <Text>{order.menuName}</Text>
-                  <Text>{order.menuPrice}</Text>
-                  <Text>{order.quantity}</Text>
+                  <Text flex="3">{(index + 1).toString().padStart(2, '0')}</Text>
+                  <Text flex="3">{order.menuName}</Text>
+                  <Text flex="2">{order.menuPrice}</Text>
+                  <Text flex="2" align="end">{order.quantity}</Text>
                 </HStack>
           </Box>
         ))}
@@ -67,12 +64,13 @@ export const ReceiptPage: React.FC<ReceiptPageProps> = () => {
             <Text {...textStyles.h3}>{receipt?.totalAmount}</Text>
         </VStack>
         </HStack>
+       
         <Box borderTop="1px dotted" mt={2} mb={2} />
+        <Center>
         <Flex align="center" justify="center" >
         <Box
         position="fixed"
         bottom="4"
-        left="32%"
         width="109px"
         height="29px"
         textAlign="center"
@@ -82,7 +80,9 @@ export const ReceiptPage: React.FC<ReceiptPageProps> = () => {
          
         </Box>
         </Flex>
+        </Center>
     </Box>
+    </Center>
   );
 };
 
