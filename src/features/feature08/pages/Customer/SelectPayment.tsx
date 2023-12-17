577b18b546439904e264da943dc25fef9f18f232
Chayapol Mahatthanachai <best37@hotmail.com>
2023-12-17 18:22:13 +0700

    [UPDATE] SelectPayment
    update checkout redirect

--- src/features/feature08/pages/Customer/SelectPayment.tsx
+++ src/features/feature08/pages/Customer/SelectPayment.tsx
@@ -2,13 +2,18 @@ import { QrCodeButton } from "../QrCode/QrCodeButton";
 import { MobileBankingList } from "../MobileBanking/MobileBankingList"
 import { CreditCardList } from "../CreditCard/CreditCardList";
 import { ConfirmButton } from "../Confirm/ConfirmButton";
-import { Box, Button, ChakraProvider, FormControl, FormLabel } from "@chakra-ui/react";
+import { Box, Button,} from "@chakra-ui/react";
 import {MdAttachMoney} from "react-icons/md"
 import { FC, useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import PayButton from "./PayButton";
+
+import { loadStripe } from '@stripe/stripe-js';
+import { Axios } from "../../../../AxiosInstance";
+
+const stripePromise = loadStripe('pk_test_51OFf98BCLtNTpQNyKo7pOR2Oyl2N3LxLtvGO549ogZUwpqgAUY0ycFgCYGhJbNXXnnyy1eLxTC2czmCuZqRd5BKy00lHA8sWfw'); // replace 'your_publishable_key' with your actual publishable key
 
 interface ButtonProps {
   bgColor?: string;
@@ -36,6 +41,48 @@ export const SelectPayment: FC<ButtonProps>= ({
 }) => {
   const [creditCardUser, setCreditCardUser] = useState<creditCardUser[]>([]);
   const { userId } = useParams();
+
+  const redirectToCheckout = async (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log("hello")
+
+    // const stripe = await stripePromise;
+
+    // Call your server's endpoint to create a checkout session
+    // const response = await fetch("http://localhost:4000/create-checkout-session", {
+    //   method: "POST",
+    // });
+
+    
+    Axios.post('/feature8/create-checkout-session',{} ).then(async (res)=> {
+      // const session = res;
+      // const result = await stripe?.redirectToCheckout({
+      //   sessionId: res.data.id,
+      // });
+  
+      // // If redirectToCheckout fails due to a browser or network error, you should display the localized error message to your customer
+      // if (result.error) {
+      //   alert(result.error.message);
+      // }
+      console.log(res.data)
+      window.location.href = res.data.url
+    }).catch((err)=> {
+      console.log(err)
+      throw err;
+    })
+
+    // const session = await response.json();
+
+    // Redirect to Checkout
+    // const result = await stripe.redirectToCheckout({
+    //   sessionId: session.id,
+    // });
+
+    // // If redirectToCheckout fails due to a browser or network error, you should display the localized error message to your customer
+    // if (result.error) {
+    //   alert(result.error.message);
+    // }
+  };
 
   // const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
   //   e.preventDefault();
@@ -78,8 +125,8 @@ export const SelectPayment: FC<ButtonProps>= ({
         alignItems={"center"}
         margin={5}
       >
-      <form action="/create-checkout-session" method="POST">
-        <Button
+      {/* <form onSubmit={redirectToCheckout}> */}
+        {/* <Button
               type="submit"
               id="checkout-button"
             width={"70%"}
@@ -92,8 +139,11 @@ export const SelectPayment: FC<ButtonProps>= ({
             leftIcon={<MdAttachMoney />}
             >
             Cash
-          </Button>
-      </form>
+        </Button> */}
+        {/* <button type="submit">Check Out</button> */}
+                <Button onClick={redirectToCheckout}>Check Out</Button>
+
+      {/* </form> */}
           
         {/* <PayButton cartItems={[]} /> */}
         <QrCodeButton />
