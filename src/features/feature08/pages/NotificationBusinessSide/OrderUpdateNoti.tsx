be326e86ee7be419338f59644bc455955c3d769d
3hiroha <97663526+3hiroha@users.noreply.github.com>
2023-12-18 01:17:20 +0700

    edit orderUpdateNoti
    comment out the nonused part
    * plz remain the comment

--- src/features/feature08/pages/NotificationBusinessSide/OrderUpdateNoti.tsx
+++ src/features/feature08/pages/NotificationBusinessSide/OrderUpdateNoti.tsx
@@ -2,8 +2,8 @@
 import {
   Table,
   Tbody,
-  Tr,
-  Td,
+  // Tr,
+  // Td,
   TableContainer,
   Card,
   Heading,
@@ -17,50 +17,52 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-interface Order  {
-  userId: number;
-  venueId: number;
-  order_date: Date;
-  total_amount: number;
-  addressId: null;
-  branchId: number;
-  driverId: null;
-  isDelivery: boolean;
-  orderId: number;
-  status: string;
-  reserveId: null;
-}
+// interface Order  {
+//   userId: number;
+//   venueId: number;
+//   order_date: Date;
+//   total_amount: number;
+//   addressId: null;
+//   branchId: number;
+//   driverId: null;
+//   isDelivery: boolean;
+//   orderId: number;
+//   status: string;
+//   reserveId: null;
+// }
 
 export const OrderUpdateNoti = () => {
-  const [userData, setUserData] = useState('');
-  const [userId, setUserId] = useState('');
+  // const [userData, setUserData] = useState('');
+  // const [userId, setUserId] = useState('');
   const [order, setOrder] = useState([]);
   const [loading, setLoading] = useState(true);
   const { orderId } = useParams();
-            
-  const fetchData = async () => {
-    try {
-      const backendUrl = import.meta.env.VITE_BACKEND_URL;
-      const response = await fetch(backendUrl + '/feature8/user', {
-        credentials: 'include'
-      });
+     
+  
+  // Idea on fetching userId by credential cookies
+  // const fetchData = async () => {
+  //   try {
+  //     const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  //     const response = await fetch(backendUrl + '/feature8/user', {
+  //       credentials: 'include'
+  //     });
 
-      if (response.ok) {
-        const userData = await response.json();
-        if (userData) {
-          console.log('API Response:', userData);
-          setUserData(userData);
-          setUserId(userData.userId);
-        } else { 
-          console.error('No user data received from API');
-        }
-      } else {
-        console.error('Error fetching user data. Status:', response.status);
-      }
-    } catch (error) {
-      console.error('Error fetching user data:', error);
-    }
-  };
+  //     if (response.ok) {
+  //       const userData = await response.json();
+  //       if (userData) {
+  //         console.log('API Response:', userData);
+  //         setUserData(userData);
+  //         setUserId(userData.userId);
+  //       } else { 
+  //         console.error('No user data received from API');
+  //       }
+  //     } else {
+  //       console.error('Error fetching user data. Status:', response.status);
+  //     }
+  //   } catch (error) {
+  //     console.error('Error fetching user data:', error);
+  //   }
+  // };
 
   const fetchOrderData = async () => {
     try {
@@ -77,7 +79,7 @@ export const OrderUpdateNoti = () => {
 
   useEffect(() => {
     fetchOrderData();
-    fetchData();
+    // fetchData();
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [orderId]); // Include orderId as a dependency to re-run the effect when it changes
 
