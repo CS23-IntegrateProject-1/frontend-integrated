ba2c439c7d562ce2715c2f62e57625cb1335fad4
Chotiwet Wisitworanat <chotiwet.wisi@icloud.com>
2023-12-18 04:41:35 +0700

    Fix stroke property (React refresh error)

--- src/features/feature07/component/ImageUpload/FigImage.tsx
+++ src/features/feature07/component/ImageUpload/FigImage.tsx
@@ -11,10 +11,10 @@ interface FigImageProps {
 const FigImage: FC<FigImageProps> = ( ) => (
     <svg xmlns="http://www.w3.org/2000/svg" width="32" height="31" viewBox="0 0 32 31" fill="none">
     <path d="M31 16C31 15.1715 30.3284 14.5 29.5 14.5C28.6716 14.5 28 15.1715 28 16H31ZM16 4C16.8285 4 17.5 3.32843 17.5 2.5C17.5 1.67157 16.8285 1 16 1V4ZM27.25 28H4.75V31H27.25V28ZM4 27.25V4.75H1V27.25H4ZM28 16V27.25H31V16H28ZM4.75 4H16V1H4.75V4ZM4.75 28C4.33579 28 4 27.6642 4 27.25H1C1 29.3211 2.67893 31 4.75 31V28ZM27.25 31C29.3211 31 31 29.3211 31 27.25H28C28 27.6642 27.6642 28 27.25 28V31ZM4 4.75C4 4.33579 4.33578 4 4.75 4V1C2.67894 1 1 2.67893 1 4.75H4Z" fill="black"/>
-    <path d="M2.5 24.2497L10.5198 16.8982C11.0792 16.3854 11.9334 16.3712 12.5096 16.865L22 24.9997" stroke="black" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
-    <path d="M19 21.2499L22.5801 17.6698C23.1079 17.142 23.9435 17.0826 24.5408 17.5305L29.5 21.2499" stroke="black" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
-    <path d="M25.75 11.5V2.5" stroke="black" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
-    <path d="M22 6.25L25.75 2.5L29.5 6.25" stroke="black" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M2.5 24.2497L10.5198 16.8982C11.0792 16.3854 11.9334 16.3712 12.5096 16.865L22 24.9997" stroke="black" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/>
+    <path d="M19 21.2499L22.5801 17.6698C23.1079 17.142 23.9435 17.0826 24.5408 17.5305L29.5 21.2499" stroke="black" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/>
+    <path d="M25.75 11.5V2.5" stroke="black" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/>
+    <path d="M22 6.25L25.75 2.5L29.5 6.25" stroke="black" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/>
     </svg>
     
 );
