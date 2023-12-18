b02575401416581f0098d1f141481291894f72ee
jjustmint <123088410+jjustmint@users.noreply.github.com>
2023-12-18 14:14:20 +0700

    Update Reservation.tsx

--- src/features/feature06/pages/Business/Reservation.tsx
+++ src/features/feature06/pages/Business/Reservation.tsx
@@ -47,11 +47,8 @@ export const Reservation = () => {
     offline: true,
     online: true,
   });
+   const [selectedDate, setSelectedDate] = useState<string | null>(null);
 
-  setFilterOptions({
-    offline: true,
-    online: true,
-  })
   
   useEffect(() => {
     fetchData();
