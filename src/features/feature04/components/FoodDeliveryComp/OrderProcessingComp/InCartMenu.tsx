5acf9bdcb0f3e6ae4d2832c823b8246eb3bf49f5
Kaung Myat Kyaw <63942523+KMKyaw@users.noreply.github.com>
2023-12-18 23:32:25 +0700

    cart cookie

--- src/features/feature04/components/FoodDeliveryComp/OrderProcessingComp/InCartMenu.tsx
+++ src/features/feature04/components/FoodDeliveryComp/OrderProcessingComp/InCartMenu.tsx
@@ -1,65 +1,116 @@
+import React, { useState } from "react";
 import { Box, Flex, Text, IconButton, Divider, Button } from "@chakra-ui/react";
 import { AddIcon, MinusIcon } from "@chakra-ui/icons";
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import index from "../../../../../theme/foundations/index";
+import { Axios } from "../../../../../AxiosInstance";
+import { useQuery } from "@tanstack/react-query";
+
 export const InCartMenu = () => {
   const [addItem, setAddItem] = useState(0);
   const handleAddItem = () => {
     setAddItem(addItem + 1);
   };
+
   const handleDecreaseItem = () => {
     if (addItem > 1) {
       setAddItem(addItem - 1);
     }
   };
+
   const navigate = useNavigate();
-  const nav=()=>{
-    navigate('/map/food-delivery/checkout');
-  }
+
+  const nav = () => {
+    navigate("/map/food-delivery/checkout");
+  };
+
+  const fetchCartItems = async () => {
+    try {
+      const response = await Axios.get("/feature4/showOrderCart");
+      console.log("Response:", response.data);
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching cart items:", error);
+      console.log("Error response:");
+    }
+  };
+
+  const {
+    data: cartItems,
+    isLoading,
+    isError,
+  } = useQuery(["cartItem"], () => fetchCartItems());
+
+  const calculateSubtotal = (items: typeof cartItems) => {
+    if (!items) return 0; // Check if items is undefined or null
+
+    return items.reduce((acc: number, item: typeof cartItems) => {
+      const price = parseFloat(item.price);
+      const quantity = parseInt(item.quantity, 10);
+
+      console.log(`Price: ${price}, Quantity: ${quantity}`);
+
+      return acc + price * quantity;
+    }, 0);
+  };
+
+  const calculateTotal = (items: typeof cartItems) => {
+    const subtotal = calculateSubtotal(items);
+    const total = parseFloat(subtotal) + 0; // Add any additional fees or adjustments here
+    return total.toFixed(2); // Round to two decimal places
+  };
+
   return (
     <Box mt={20}>
       <Box>
-        <Flex flexDir={"row"} justifyContent={"space-around"} border={"solid 1.5px #DEBEF6"} borderRadius={10}p={3}>
-
-          <img
-            src="https://www.mkrestaurant.com/public/uploads/mk_menu/images/33e10dd680609fd2de8cc182fd51f644.jpg"
-            width="10%"
-            height="10%"
-            style={{ borderRadius: "20px" }}
-            alt="Menu Item"
-          />
-          <Flex flexDir={"column"}>
-            <Text>MK Roasted Duck</Text>
-            <Text>small</Text>
-          </Flex>
-          <Flex flexDir={"column"} justifyContent={"space-around"}>
-            <Text>$20</Text>
-            <Flex flexDir={"row"}>
-              <IconButton
-                icon={<MinusIcon />}
-                onClick={handleDecreaseItem}
-                isDisabled={addItem === 0}
-                aria-label="Decrease Amount"
-                width="30px"
-                height="28px"
-                borderRadius="10% 0% 0% 10%"
+        {cartItems &&
+          cartItems.map((item: typeof cartItems, index: number) => (
+            <Flex
+              key={index}
+              flexDir={"row"}
+              justifyContent={"space-around"}
+              border={"solid 1.5px #DEBEF6"}
+              borderRadius={10}
+              p={3}
+            >
+              <img
+                src="https://www.mkrestaurant.com/public/uploads/mk_menu/images/33e10dd680609fd2de8cc182fd51f644.jpg"
+                width="10%"
+                height="10%"
+                style={{ borderRadius: "20px" }}
+                alt={item.name}
               />
-              <Text color="black" backgroundColor={"white"} pl={2} pr={2}>
-                {addItem}
-              </Text>
-              <IconButton
-                icon={<AddIcon />}
-                onClick={handleAddItem}
-                aria-label="Add Item"
-                width="30px"
-                height="28px"
-                borderRadius="0 10% 10% 0"
-              />
+              <Flex flexDir={"column"}>
+                <Text>{item.name}</Text>
+                <Text>{item.size}</Text>
+              </Flex>
+              <Flex flexDir={"column"} justifyContent={"space-around"}>
+                <Text>${item.price}</Text>
+                <Flex flexDir={"row"}>
+                  <IconButton
+                    icon={<MinusIcon />}
+                    onClick={handleDecreaseItem}
+                    isDisabled={addItem === 0}
+                    aria-label="Decrease Amount"
+                    width="30px"
+                    height="28px"
+                    borderRadius="10% 0% 0% 10%"
+                  />
+                  <Text color="black" backgroundColor={"white"} pl={2} pr={2}>
+                    {item.quantity}
+                  </Text>
+                  <IconButton
+                    icon={<AddIcon />}
+                    onClick={handleAddItem}
+                    aria-label="Add Item"
+                    width="30px"
+                    height="28px"
+                    borderRadius="0 10% 10% 0"
+                  />
+                </Flex>
+              </Flex>
             </Flex>
-          </Flex>
-        </Flex>
-        <Divider borderColor={index.colors.brand[100]} mt={5} />
+          ))}
+        <Divider borderColor="#DEBEF6" mt={5} />
         <Flex
           flexDirection={"row"}
           alignItems={"center"}
@@ -72,7 +123,7 @@ export const InCartMenu = () => {
             width={500}
             borderRadius={10}
             border={"solid 1.5px"}
-            borderColor={index.colors.brand[100]}
+            borderColor="#DEBEF6"
             p={5}
             mt={5}
             mb={5}
@@ -83,7 +134,7 @@ export const InCartMenu = () => {
               justifyContent={"space-around"}
             >
               <Text>Subtotal</Text>
-              <Text>$20</Text>
+              <Text>${calculateSubtotal(cartItems)}</Text>
             </Box>
             <Box
               display={"flex"}
@@ -96,34 +147,35 @@ export const InCartMenu = () => {
           </Box>
         </Flex>
       </Box>
-        <Flex
+      <Flex
+        flexDirection={"row"}
+        alignItems={"center"}
+        justifyContent={"space-around"}
+        m={2}
+      >
+        <Box
+          display={"flex"}
           flexDirection={"row"}
-          alignItems={"center"}
           justifyContent={"space-around"}
-          m={2}
+          gap={2}
         >
-          <Box
-            display={"flex"}
-            flexDirection={"row"}
-            justifyContent={"space-around"}
-            gap={2}
-          >
-            <Text>Total(incl. Vat)</Text>
-            <Text>$20</Text>
-          </Box>
-          <Button
-            variant={"unstyle"}
-            backgroundColor={index.colors.brand[200]}
-            maxWidth={500}
-            minWidth={100}
-            maxHeight={100}
-            width={500}
-            mb={2}
-            onClick={nav}
-          >
-            Review payment and address
-          </Button>
-        </Flex>
+          <Text>Total (incl. Vat)</Text>
+          <Text>${calculateTotal(cartItems)}</Text>
+        </Box>
+        <Button
+          variant={"unstyled"}
+          backgroundColor="#805AD5"
+          color="white"
+          maxWidth={500}
+          minWidth={100}
+          maxHeight={100}
+          width={500}
+          mb={2}
+          onClick={nav}
+        >
+          Review payment and address
+        </Button>
+      </Flex>
     </Box>
   );
 };
