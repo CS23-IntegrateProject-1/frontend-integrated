6a24615f60d44e74b2a0a6f7861978a1b4ce8bbd
jjustmint <123088410+jjustmint@users.noreply.github.com>
2023-12-22 00:33:02 +0700

    Update ReservationDetail.tsx

--- src/features/feature06/pages/ReservationDetail.tsx
+++ src/features/feature06/pages/ReservationDetail.tsx
@@ -86,9 +86,6 @@ export const ReservationDetail = () => {
       if (selectedDateTime < currentDate) {
         toast.warning("Please select a future date and time");
         return;
-      }else if (phonenumber.length > 10){
-        toast.warning("Please enter a valid phone number");
-        return;
       }
       if (date == "" || time == "" || name == "" || phonenumber == "") {
         toast.warning("Please fill in all information");
@@ -208,6 +205,7 @@ export const ReservationDetail = () => {
             <Box mt={"5px"}>
               <Input
                 required
+                maxLength={10}
                 type="number"
                 placeholder="enter phone no."
                 htmlSize={4}
