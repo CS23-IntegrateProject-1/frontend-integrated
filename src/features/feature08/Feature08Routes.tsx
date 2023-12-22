fc146f1bfddbf92199e7f814e293f1dcd60e1081
tartartar <112954170+itsChewin@users.noreply.github.com>
2023-12-22 15:37:25 +0700

    [ADD] paymentDe

--- src/features/feature08/Feature08Routes.tsx
+++ src/features/feature08/Feature08Routes.tsx
@@ -1,6 +1,7 @@
 import { SelectPayment } from "./pages/Customer/SelectPayment";
 import { SelectPaymentD } from "./pages/Customer/SelectPaymentD";
 import { SelectPaymentS } from "./pages/Customer/SelectPaymentS";
+import { SelectPaymentDe } from "./pages/Customer/SelectPaymentDe";
 import { QrCodeScan } from "./pages/QrCode/QrCodeScan";
 import { AddCard } from "./pages/AddCard/AddCard";
 import { PaymentHistory } from "./pages/Payment/PaymentHistory";
@@ -23,6 +24,10 @@ export const Feature08Routes = () => {
 		{
 			path: "/venue/paymentS",
 			element: <SelectPaymentS />,
+		},
+		{
+			path: "/venue/paymentDe",
+			element: <SelectPaymentDe />,
 		},
 		{
 			path: "/venue/:venueId/payment/checkout",
