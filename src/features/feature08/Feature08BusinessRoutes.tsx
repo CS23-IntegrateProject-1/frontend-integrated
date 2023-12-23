88b9c9f74c8ca0c9c2ab5dc426bd184fde4ce538
3hiroha <97663526+3hiroha@users.noreply.github.com>
2023-12-23 21:09:36 +0700

    Edit  route & advertiseNoti

--- src/features/feature08/Feature08BusinessRoutes.tsx
+++ src/features/feature08/Feature08BusinessRoutes.tsx
@@ -18,6 +18,7 @@ import { UpdateNoti } from "./pages/NotificationBusinessSide/UpdateNoti";
 import { AddCardNoti } from "./pages/NotificationBusinessSide/AddCardNoti";
 import { EnterCode } from "./pages/NotificationBusinessSide/EnterCode";
 import { SelectPaymentAd } from "./pages/Business/SelectPaymentAd";
+import { AdvertiseNotiSuccess } from "./pages/NotificationBusinessSide/AdvertisesNotiSuccess";
 
 export const FeatureBusiness08Routes = () => {
 	return [
@@ -81,6 +82,10 @@ export const FeatureBusiness08Routes = () => {
 		{
 			path: "/business/Notification/NewReservation/:venueId/:reservationId",
 			element: <NewReserveNoti />,
+		},
+		{
+			path: "/business/Notification/advertisement/:advertisementId/:sessionId",
+			element: <AdvertiseNotiSuccess />,
 		},
 		{
 			path: "/venue/paymentAd/:advertisementId",
