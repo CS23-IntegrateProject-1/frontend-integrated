007edb9770c8c35f213bff4cee21f3fa09374443
Jirawat Rongsupan <74420545+LorZangel@users.noreply.github.com>
2023-12-24 17:02:38 +0700

    Update Feature14Routes.tsx

--- src/features/feature14/Feature14Routes.tsx
+++ src/features/feature14/Feature14Routes.tsx
@@ -1,23 +1,12 @@
-import Dashboard from "./admin/dashboard";
-import AccountEditPage from "./admin/account/editaccount";
-import AccountSetupPage from "./admin/account/setupaccount";
-import RequestPage from "./admin/adsreview/requestapproval";
-import RejectPage from "./admin/adsreview/reviewrejected";
-import AdsreviewPage from "./admin/adsreview/adsreview";
+import { VoucherListPage } from './admin/voucher/VoucherListPage'
+import { PromotionListPage } from './admin/promotion/PromotionPage'
+import { VoucherRejectPage } from './admin/voucher/VoucherRejectPage'
+import { ReportListPage } from './admin/reportAd/ReportListPage'
 export const Feature14Routes = () => {
   return [
-    { path: "/admin/dashboard", element: <Dashboard />},
-    { path: "/admin/account/edit", element: <AccountEditPage /> },
-    { path: "/admin/account/setup", element: <AccountSetupPage /> },
-    { path: "/admin/approval", element: <RequestPage /> },
-    { path: "/admin/approval/:approvalId", element: <AdsreviewPage /> },
-    {
-      path: "/admin/approval/:approvalId/reject",
-      element: <RejectPage />,
-    },
-    {
-      path: "/admin/notification/:notificationId",
-      element: "ElementPlaceHolder",
-    },
-  ];
-};
+    { path: '/admin/ticket', element: <ReportListPage /> },
+    { path: '/admin/voucher', element: <VoucherListPage /> },
+    { path: '/admin/voucher/:voucherId/reject', element: <VoucherRejectPage /> },
+    { path: '/admin/promotion', element: <PromotionListPage /> },
+  ]
+}
