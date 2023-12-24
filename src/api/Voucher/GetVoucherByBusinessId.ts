d5559110931807b6318ff62d9f2f0b7fc6e0fc2e
Jirawat Rongsupan <74420545+LorZangel@users.noreply.github.com>
2023-12-24 18:57:34 +0700

    Add files via upload

--- src/api/Voucher/GetVoucherByBusinessId.ts
+++ src/api/Voucher/GetVoucherByBusinessId.ts
@@ -2,7 +2,7 @@ import { Axios } from "../../AxiosInstance";
 
 export const GetVoucherByBusinessId = async () => {
   try {
-    const response = await Axios.get(`/feature5/AllVoucher/`);
+    const response = await Axios.get(`/feature14/getInProgressVoucher`);
     return response.data;
   } catch (error) {
     console.log(error);
